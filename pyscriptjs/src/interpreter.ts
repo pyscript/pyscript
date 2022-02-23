f65a6ec5c9ba0aaf1cbb324c77769ea9908841fb
Fabio Pliger <fabio.pliger@gmail.com>
2022-02-23 12:11:44 -0600

    add interpreter

---
+++ pyscriptjs/src/interpreter.ts
@@ -0,0 +1,13 @@
+// @ts-nocheck
+// @ts-ignore
+
+let loadInterpreter = async function(): any {
+    /* @ts-ignore */
+    let pyodide = await loadPyodide({ 
+          indexURL: "https://cdn.jsdelivr.net/pyodide/v0.19.0/full/",
+        }); 
+    /* @ts-ignore */
+    return pyodide;
+}
+
+export {loadInterpreter}
