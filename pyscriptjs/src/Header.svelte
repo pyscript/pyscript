50b632555c5b2ee33cb82343e2008fd551037006
Fabio Pliger <fabio.pliger@gmail.com>
2022-03-08 11:31:35 -0600

    replace bell with settings icon

--- pyscriptjs/src/Header.svelte
+++ pyscriptjs/src/Header.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
-    export let name = "PyScript";
-    export let editMode = true;
+  import Fa from 'svelte-fa'
+  import { faCog } from '@fortawesome/free-solid-svg-icons'
 
-    $: modeLabel = (editMode == true) ? "edit" : "run";
+  export let name = "PyScript";
+  export let editMode = true;
 
-    function toggleMode() {
+  $: modeLabel = (editMode == true) ? "edit" : "run";
+
+  function toggleMode() {
       editMode = ! editMode;
 	}
 
@@ -54,10 +57,8 @@
             <div class="ml-3 relative">
               <button type="button" class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
                 <span class="sr-only">View notifications</span>
-                <!-- Heroicon name: outline/bell -->
-                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-                </svg>
+                <!-- Settings Icon -->
+                <Fa icon={faCog} />
               </button>
               <!-- <div>
                 <button type="button" class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
