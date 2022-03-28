03da071a631df9bb0ca39a6a2476e1c8459d6ad6
Fabio Pliger <fabio.pliger@gmail.com>
2022-03-28 17:59:19 -0500

    redirect default output to console.log

--- pyscriptjs/src/interpreter.ts
+++ pyscriptjs/src/interpreter.ts
@@ -102,6 +102,7 @@ let loadInterpreter = async function(): any {
     /* @ts-ignore */
     let pyodide = await loadPyodide({ 
           indexURL: "https://cdn.jsdelivr.net/pyodide/v0.19.0/full/",
+          stdout: console.log
         }); 
 
     // now that we loaded, add additional convenience fuctions
