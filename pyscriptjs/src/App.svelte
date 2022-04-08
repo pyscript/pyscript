7fd2b56bfe09bdb5cd1de7225b84f036d9143b90
Fabio Pliger <fabio.pliger@gmail.com>
2022-04-08 18:19:34 -0500

    remove old files and old code references

--- pyscriptjs/src/App.svelte
+++ pyscriptjs/src/App.svelte
@@ -1,15 +1,8 @@
 <script lang="ts">
 
-  import Fa from 'svelte-fa';
-  import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
   import Tailwind from "./Tailwind.svelte";
   import { loadInterpreter } from './interpreter';
   import { pyodideLoaded, loadedEnvironments, navBarOpen, componentsNavOpen, mode, scriptsQueue, initializers, postInitializers } from './stores';
-  import Main from "./Main.svelte";
-  import Header from "./Header.svelte";
-  import SideNav from "./SideNav.svelte";
-  import ComponentsNav from "./ComponentsNav.svelte";
-  import ComponentDetailsNav from "./ComponentDetailsNav.svelte";
 
   let iconSize = 2;
   let pyodideReadyPromise
@@ -71,43 +64,4 @@
 	<script src="https://cdn.jsdelivr.net/pyodide/v0.19.0/full/pyodide.js" on:load={initializePyodide}></script>
 </svelte:head>
 
-<style>
-  :global(div.dev-buttons-group) {
-    margin-top: -15px;
-    z-index: 9999;
-  }
-
-  :global(div.output) {
-    z-index: 9999;
-  }
-
-  :global(div.buttons-box) {
-    visibility: hidden;
-  }
-
-  :global(.parentBox:hover .buttons-box) {
-    visibility: visible;
-  }
-
-</style>
-
 <Tailwind />
-<!--
-<div class="flex flex-wrap bg-grey-light min-h-screen">
-  <div>
-    <SideNav />
-  </div>
-  <div>
-    <ComponentsNav />
-  </div>
-  <div><ComponentDetailsNav /></div>
-  <div id="main" class="w-full min-h-full absolute pin-r flex flex-col">
-    <Header />
-    <div id="add-component" class="lex flex-column w-full text-lg">
-      <div style="margin-left: 50%;" on:mouseenter={bumpSize} on:mouseleave={downSize} on:click={toggleComponentsNavBar}>
-        <Fa icon={faPlusCircle} class="grow-icon"style="transform: scale({iconSize});"/>
-      </div>
-    </div>
-    <Main />
-  </div>
-</div> -->
