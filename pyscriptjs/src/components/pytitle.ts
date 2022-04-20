3aa3ba02be47dbfcfb2b5e6eee5f4ef1922a97d2
Fabio Pliger <fabio.pliger@gmail.com>
2022-04-20 15:41:51 -0500

    add pybutton and pytitle

---
+++ pyscriptjs/src/components/pytitle.ts
@@ -0,0 +1,37 @@
+import { BaseEvalElement } from './base';
+import { addClasses, ltrim, htmlDecode } from '../utils';
+
+export class PyTitle extends BaseEvalElement {
+    shadow: ShadowRoot;
+    wrapper: HTMLElement;
+    theme: string;
+    widths: Array<string>;
+    label: string;
+    mount_name: string;
+    constructor() {
+        super();
+      }
+
+
+    connectedCallback() {
+      this.label = htmlDecode(this.innerHTML);
+      this.mount_name  = this.id.split("-").join("_");
+      this.innerHTML = '';
+      
+      let mainDiv = document.createElement('div');
+      let divContent = document.createElement('h1')
+      
+      addClasses(mainDiv, ["text-center", "w-full", "mb-8"]);
+      addClasses(divContent, ["text-3xl", "font-bold", "text-gray-800", "uppercase", "tracking-tight"]);
+      divContent.innerHTML = this.label;
+      
+      mainDiv.id = this.id;
+      this.id = `${this.id}-container`;
+      mainDiv.appendChild(divContent);
+      this.appendChild(mainDiv);
+        
+      console.log('py-title connected');
+    }
+  }
+
+  
--- pyscriptjs/src/main.ts
+++ pyscriptjs/src/main.ts
@@ -4,12 +4,16 @@ import { PyScript } from "./components/pyscript";
 import { PyRepl } from "./components/pyrepl";
 import { PyEnv } from "./components/pyenv";
 import { PyBox } from "./components/pybox";
+import { PyButton } from "./components/pybutton";
+import { PyTitle } from "./components/pytitle";
 import { PyWidget } from "./components/base";
 
 let xPyScript = customElements.define('py-script', PyScript);
 let xPyRepl = customElements.define('py-repl', PyRepl);
 let xPyEnv = customElements.define('py-env', PyEnv);
 let xPyBox = customElements.define('py-box', PyBox);
+let xPyButton = customElements.define('py-button', PyButton);
+let xPyTitle = customElements.define('py-title', PyTitle);
 let xPyWidget = customElements.define('py-register-widget', PyWidget);
 
 
