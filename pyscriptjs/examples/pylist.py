ea0cddee963d92a2ebbeb1e30927d25ef0477ab6
Fabio Pliger <fabio.pliger@gmail.com>
2022-04-20 13:48:11 -0500

    simplify the example

--- pyscriptjs/examples/pylist.py
+++ pyscriptjs/examples/pylist.py
@@ -13,10 +13,7 @@ class PyItem(PyItemTemplate):
     self.strike(self.data['done'])
   
     self.select('input').element.checked = self.data['done']
-
-  def display(self):
-    return self.data['content']
-
+    
 
 def add_task(*ags, **kws):
   task = { "content": new_task_content.value,  "done": False, "created_at": dt.now() }
