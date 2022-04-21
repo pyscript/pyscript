69f39713e018f283f3cdf4c79bc0772cea562773
Fabio Pliger <fabio.pliger@gmail.com>
2022-04-20 20:53:31 -0500

    move code from pylist.py to the example directly to make pylist reusable and show more of the example potential

--- pyscriptjs/examples/pylist.py
+++ pyscriptjs/examples/pylist.py
@@ -9,14 +9,3 @@ class PyItem(PyItemTemplate):
     
 class PyList(PyListTemplate):
   item_class = PyItem
-
-def add_task(*ags, **kws):
-  # create a new dictionary representing the new task
-  task = { "content": new_task_content.value,  "done": False, "created_at": dt.now() }
-
-  # add a new task to the list and tell it to use the `content` key to show in the UI
-  # and to use the key `done` to sync the task status with a checkbox element in the UI
-  myList.add(task, labels=['content'], state_key="done")
-
-  # clear the inputbox element used to create the new task
-  new_task_content.clear()
