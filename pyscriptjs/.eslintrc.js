6f8f978ef7d663f5f1098280a226e0df80f12cb2
Ross Bermudez <rbermudez@anaconda.com>
2022-04-25 14:09:41 +0200

    [PYS-12] Add eslint and prettier

---
+++ pyscriptjs/.eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking'
+    ],
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+        extraFileExtensions: ['.svelte']
+    },
+    env: {
+        es6: true,
+        browser: true
+    },
+    overrides: [
+        {
+            files: ['*.svelte'],
+            processor: 'svelte3/svelte3'
+        }
+    ],
+    settings: {
+        'svelte3/typescript': require('typescript'),
+        // ignore style tags in Svelte because of Tailwind CSS
+        // See https://github.com/sveltejs/eslint-plugin-svelte3/issues/70
+        'svelte3/ignore-styles': () => true
+    },
+    plugins: ['svelte3', '@typescript-eslint'],
+    ignorePatterns: ['node_modules'],
+}
---
+++ pyscriptjs/.prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    singleQuote: true,
+    printWidth: 120,
+    plugins: ['prettier-plugin-svelte'],
+    semi: false,
+    svelteSortOrder: 'options-styles-scripts-markup',
+    svelteStrictMode: false,
+    svelteIndentScriptAndStyle: true,
+    tabWidth: 2,
+    trailingComma: 'all',
+}
