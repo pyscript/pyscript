6de9f11f5a1a06ca9d10b8c61dbc405599c43fd6
rajat.sahu <rajat.sahu@gojek.com>
2022-05-03 14:09:28 +0530

    [Rajat] added example for fetch api (using bpi price api)

---
+++ pyscriptjs/examples/fetch_data.py
@@ -0,0 +1,37 @@
+from pydoc import doc
+from js import console, fetch, JSON , document
+import json
+
+async def get_data():
+    res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+    data = json.loads(JSON.stringify(await res.json()))
+    console.log('fetched data from api ',data)
+    return data['bpi']
+
+
+async def render():
+    data = await get_data()
+    for currency in data:
+        col = document.createElement('div')
+        col.setAttribute('class','col-2')
+        
+        card_div = document.createElement('div')
+        card_div.setAttribute('class','card text-center shadow  mb-5 bg-white rounded')
+        
+        card_header = document.createElement('div')
+        card_header.setAttribute('class','card-header bg-warning bg-gradient mb-3')
+        card_header.innerHTML = data[currency]['symbol'] + ' ' + data[currency]['rate']
+        card_div.appendChild(card_header)
+
+        card_body = document.createElement('div')
+        card_body.setAttribute('class','card-body')
+        
+        card_title = document.createElement('h2')
+        card_title.setAttribute('class','card-title')
+        card_title.innerText = currency
+        card_body.appendChild(card_title)
+        
+        card_div.appendChild(card_body)
+        col.appendChild(card_div)
+
+        document.getElementById('data-row').append(col)
