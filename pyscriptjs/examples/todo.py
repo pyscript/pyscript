c5279b0a38d296e5980f0c3d6c341bb645f3eb7a
Allan Lago <allanlago1@gmail.com>
2022-05-05 16:34:13 -0400

    fix linting

--- pyscriptjs/examples/todo.py
+++ pyscriptjs/examples/todo.py
@@ -12,35 +12,42 @@ new_task_content = Element("new-task-content")
 
 
 def add_task(*ags, **kws):
-  # ignore empty task
-  if not new_task_content.element.value:
-    return None
+    # ignore empty task
+    if not new_task_content.element.value:
+      return None
+    
+    # create task
+    task_id = f"task-{len(tasks)}"
+    task = {
+        "id": task_id,
+        "content": new_task_content.element.value,
+        "done": False,
+        "created_at": dt.now(),
+    }
 
-  # create task
-  task_id = f"task-{len(tasks)}"
-  task = {"id": task_id, "content": new_task_content.element.value, "done": False, "created_at": dt.now()}
+    tasks.append(task)
 
-  tasks.append(task)
-  
-  # add the task element to the page as new node in the list by cloning from a template
-  taskHtml = task_template.clone(task_id, to=task_list)
-  taskHtmlContent = taskHtml.select('p')
-  taskHtmlContent.element.innerText = task['content']
-  taskHtmlCheck = taskHtml.select('input')
-  task_list.element.appendChild(taskHtml.element)
-  
-  def check_task(evt=None):
-    task['done'] = not task['done']
-    if task['done']:
-      add_class(taskHtmlContent, "line-through")
-    else:
-      remove_class(taskHtmlContent, "line-through")
+    # add the task element to the page as new node in the list by cloning from a template
+    taskHtml = task_template.clone(task_id, to=task_list)
+    taskHtmlContent = taskHtml.select("p")
+    taskHtmlContent.element.innerText = task["content"]
+    taskHtmlCheck = taskHtml.select("input")
+    task_list.element.appendChild(taskHtml.element)
 
-  new_task_content.clear()
-  taskHtmlCheck.element.onclick = check_task
+    def check_task(evt=None):
+        task["done"] = not task["done"]
+        if task["done"]:
+            add_class(taskHtmlContent, "line-through")
+        else:
+            remove_class(taskHtmlContent, "line-through")
+
+    new_task_content.clear()
+    taskHtmlCheck.element.onclick = check_task
+
 
 def add_task_event(e):
-  if (e.key == "Enter"):
-    add_task()
+    if e.key == "Enter":
+        add_task()
+
 
 new_task_content.element.onkeypress = add_task_event
