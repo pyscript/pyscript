7695327cbd7c62e1474e11eb496981d98b482ea1
Jason Barnwell <master.j1994@gmail.com>
2022-05-04 23:46:55 -0400

    Changed variable names to snakecase for PEP8 formatting.

--- pyscriptjs/examples/todo.py
+++ pyscriptjs/examples/todo.py
@@ -15,23 +15,23 @@ def add_task(*ags, **kws):
   task = {"id": task_id, "content": new_task_content.element.value, "done": False, "created_at": dt.now()}
 
   tasks.append(task)
-  
+
   # add the task element to the page as new node in the list by cloning from a template
-  taskHtml = task_template.clone(task_id, to=task_list)
-  taskHtmlContent = taskHtml.select('p')
-  taskHtmlContent.element.innerText = task['content']
-  taskHtmlCheck = taskHtml.select('input')
-  task_list.element.appendChild(taskHtml.element)
-  
+  task_html = task_template.clone(task_id, to=task_list)
+  task_html_content = task_html.select('p')
+  task_html_content.element.innerText = task['content']
+  task_html_check = task_html.select('input')
+  task_list.element.appendChild(task_html.element)
+
   def check_task(evt=None):
     task['done'] = not task['done']
     if task['done']:
-      add_class(taskHtmlContent, "line-through")
+      add_class(task_html_content, "line-through")
     else:
-      remove_class(taskHtmlContent, "line-through")
+      remove_class(task_html_content, "line-through")
 
   new_task_content.clear()
-  taskHtmlCheck.element.onclick = check_task
+  task_html_check.element.onclick = check_task
 
 def add_task_event(e):
   if (e.key == "Enter"):
