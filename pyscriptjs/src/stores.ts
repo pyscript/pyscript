b8556d8bdeecf6a3ac4c8c8706a13020dbd78d1e
woxtu <woxtup@gmail.com>
2022-05-06 10:58:54 +0900

    Add type annotations

--- pyscriptjs/src/stores.ts
+++ pyscriptjs/src/stores.ts
@@ -1,4 +1,7 @@
 import { writable } from 'svelte/store';
+import type { PyScript } from './components/pyscript';
+
+type Initializer = () => Promise<void>;
 
 export const pyodideLoaded = writable({
     loaded: false,
@@ -14,19 +17,19 @@ export const componentDetailsNavOpen = writable(false);
 export const mainDiv = writable(null);
 export const currentComponentDetails = writable([]);
 export const mode = writable(DEFAULT_MODE);
-export const scriptsQueue = writable([]);
-export const initializers = writable([]);
-export const postInitializers = writable([]);
+export const scriptsQueue = writable<PyScript[]>([]);
+export const initializers = writable<Initializer[]>([]);
+export const postInitializers = writable<Initializer[]>([]);
 
-let scriptsQueue_ = [];
-let initializers_ = [];
-let postInitializers_ = [];
+let scriptsQueue_: PyScript[] = [];
+let initializers_: Initializer[] = [];
+let postInitializers_: Initializer[] = [];
 
 scriptsQueue.subscribe(value => {
     scriptsQueue_ = value;
 });
 
-export const addToScriptsQueue = script => {
+export const addToScriptsQueue = (script: PyScript) => {
     scriptsQueue.set([...scriptsQueue_, script]);
 };
 
@@ -34,7 +37,7 @@ initializers.subscribe(value => {
     initializers_ = value;
 });
 
-export const addInitializer = initializer => {
+export const addInitializer = (initializer: Initializer) => {
     console.log('adding initializer', initializer);
     initializers.set([...initializers_, initializer]);
     console.log('adding initializer', initializer);
@@ -44,7 +47,7 @@ postInitializers.subscribe(value => {
     postInitializers_ = value;
 });
 
-export const addPostInitializer = initializer => {
+export const addPostInitializer = (initializer: Initializer) => {
     console.log('adding post initializer', initializer);
     postInitializers.set([...postInitializers_, initializer]);
     console.log('adding post initializer', initializer);
