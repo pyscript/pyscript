028d7cd7c401494c8929916de34bc01741b4637c
Ioannis Chrysostomakis <ic768@protonmail.com>
2022-05-10 20:56:41 +0300

    Add labels for pyrepl component

--- pyscriptjs/src/components/pyrepl.ts
+++ pyscriptjs/src/components/pyrepl.ts
@@ -95,16 +95,37 @@ export class PyRepl extends BaseEvalElement {
         });
 
         const mainDiv = document.createElement('div');
+        mainDiv.id = 'code-editor';
         addClasses(mainDiv, ['parentBox', 'flex', 'flex-col', 'mt-2', 'mx-8', 'relative']);
+
+        // Styles that we use to hide the labels whilst also keeping it accessible for screen readers
+        const labelStyle = 'overflow:hidden; display:block; width:1px; height:1px';
+
+        // Code editor Label
+        const editorLabel = document.createElement('label');
+        editorLabel.innerHTML = 'Python Script Area';
+        editorLabel.setAttribute('style', labelStyle);
+        editorLabel.htmlFor = 'code-editor';
+
+        mainDiv.append(editorLabel);
 
         // add Editor to main PyScript div
         mainDiv.appendChild(this.editorNode);
 
         // Play Button
         this.btnRun = document.createElement('button');
+        this.btnRun.id = 'btnRun';
         this.btnRun.innerHTML =
             '<svg id="" class="svelte-fa svelte-ps5qeg" style="height:20px;width:20px;vertical-align:-.125em;transform-origin:center;overflow:visible;color:green" viewBox="0 0 384 512" aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg"><g transform="translate(192 256)" transform-origin="96 0"><g transform="translate(0,0) scale(1,1)"><path d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z" fill="currentColor" transform="translate(-192 -256)"></path></g></g></svg>';
         addClasses(this.btnRun, ['absolute', 'right-1', 'bottom-3', 'opacity-0', 'group-hover:opacity-100']);
+
+        // Play Button Label
+        const btnLabel = document.createElement('label');
+        btnLabel.innerHTML = 'Python Script Run Button';
+        btnLabel.setAttribute('style', labelStyle);
+        btnLabel.htmlFor = 'btnRun';
+
+        this.editorNode.appendChild(btnLabel);
         this.editorNode.appendChild(this.btnRun);
 
         this.btnRun.onclick = wrap(this);
