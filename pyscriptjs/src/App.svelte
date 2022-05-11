363f3751f9d37f56cfb1c94cdd354116112eceba
Fabio Pliger <fpliger@users.noreply.github.com>
2022-05-11 16:59:13 -0500

    support different pyodide versions (#328)
    * add PyLoader class
    
    * create global loader during app creation time and remove it when pyscript loading operations are done
    
    * make the loader global and open/close when apps is starting. Also add concept of app config so users can set if they want to autoclose the loader of handle it themselves
    
    * add pyconfig file
    
    * auto add global config if there's no config set in the page
    
    * export initializer type
    
    * define type for config
    
    * move initialization out of svelte file, into app config
    
    * change runtimes from strings to objects
    
    * fix typo

--- pyscriptjs/src/App.svelte
+++ pyscriptjs/src/App.svelte
@@ -1,76 +1,5 @@
 <script lang="ts">
     import Tailwind from './Tailwind.svelte';
-    import { loadInterpreter } from './interpreter';
-    import type { AppConfig } from './components/pyconfig';
-    import { initializers, loadedEnvironments, mode, postInitializers, pyodideLoaded, scriptsQueue, globalLoader, appConfig } from './stores';
-
-    let pyodideReadyPromise;
-
-    let loader;
-    let appConfig_: AppConfig = {
-        autoclose_loader: true,
-    };
-
-    globalLoader.subscribe(value => {
-        loader = value;
-    });
-
-    appConfig.subscribe( (value:AppConfig) => {
-        if (value){
-            appConfig_ = value;
-        }
-        console.log("config set!")
-    });
-
-    const initializePyodide = async () => {
-        loader.log("Loading runtime...")
-        pyodideReadyPromise = loadInterpreter();
-        const pyodide = await pyodideReadyPromise;
-        const newEnv = {
-            id: 'a',
-            promise: pyodideReadyPromise,
-            runtime: pyodide,
-            state: 'loading',
-        };
-        pyodideLoaded.set(pyodide);
-
-        // Inject the loader into the runtime namespace
-        pyodide.globals.set("pyscript_loader", loader);
-
-        loader.log("Runtime created...")
-        loadedEnvironments.update((value: any): any => {
-            value[newEnv['id']] = newEnv;
-        });
-
-        // now we call all initializers before we actually executed all page scripts
-        loader.log("Initializing components...")
-        for (let initializer of $initializers) {
-            await initializer();
-        }
-
-        // now we can actually execute the page scripts if we are in play mode
-        loader.log("Initializing scripts...")
-        if ($mode == 'play') {
-            for (let script of $scriptsQueue) {
-                await script.evaluate();
-            }
-            scriptsQueue.set([]);
-        }
-
-        // now we call all post initializers AFTER we actually executed all page scripts
-        loader.log("Running post initializers...");
-
-        if (appConfig_ && appConfig_.autoclose_loader) {
-            loader.close();
-            console.log("------ loader closed ------");
-        }
-
-        setTimeout(async () => {
-            for (let initializer of $postInitializers) {
-                await initializer();
-            }
-        }, 3000);
-    };
 </script>
 
 <style global>
@@ -104,9 +33,5 @@
       margin-top: 6rem;
     }
   </style>
-
-<svelte:head>
-    <script src="https://cdn.jsdelivr.net/pyodide/v0.20.0/full/pyodide.js" on:load={initializePyodide}></script>
-</svelte:head>
 
 <Tailwind />
