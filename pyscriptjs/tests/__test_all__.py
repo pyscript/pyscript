bb994875256cda8561b69f585775a42ca5cb9cf9
cleonard <ccl@chrisleonard.com>
2022-05-12 21:14:32 -0500

    Basic tests for each example in examples/index.html

---
+++ pyscriptjs/tests/__test_all__.py
@@ -0,0 +1,41 @@
+"""Run all tests in CLI mode so that the results of print() statements are displayed"""
+
+from test_hello_world import test as test_hello_world
+from test_simple_clock import test as test_simple_clock
+from test_repl import test as test_repl
+from test_repl2 import test as test_repl2
+from test_todo import test as test_todo
+from test_todo_pylist import test as test_todo_pylist
+from test_matplotlib import test as test_matplotlib
+from test_altair import test as test_altair
+from test_folium import test as test_folium
+from test_d3 import test as test_d3
+from test_webgl_raycaster_index import test as test_webgl_raycaster_index
+from test_bokeh import test as test_bokeh
+from test_bokeh_interactive import test as test_bokeh_interactive
+from test_panel_kmeans import test as test_panel_kmeans
+from test_panel_stream import test as test_panel_stream
+from test_panel import test as test_panel
+from test_panel_deckgl import test as test_panel_deckgl
+# from test_toga_freedom import test as test_toga_freedom
+from test_numpy_canvas_fractals import test as test_numpy_canvas_fractals
+
+test_hello_world()
+test_simple_clock()
+test_repl()
+test_repl2()
+test_todo()
+test_todo_pylist()
+test_matplotlib()
+test_altair()
+test_folium()
+test_d3()
+test_webgl_raycaster_index()
+test_bokeh()
+test_bokeh_interactive()
+test_panel_kmeans()
+test_panel_stream()
+test_panel()
+test_panel_deckgl()
+# test_toga_freedom()
+test_numpy_canvas_fractals()
---
+++ pyscriptjs/tests/test_hello_world.py
@@ -0,0 +1,90 @@
+"""Test: hello_world"""
+
+import math
+import re
+import time
+
+try:
+    # When running `make test`
+    from .common import (
+        MAX_TEST_TIME,
+        TEST_TIME_INCREMENT,
+        _url_join,
+    )
+except:
+    # When running `python test_<example name>.py`
+    from common import (
+        MAX_TEST_TIME,
+        TEST_TIME_INCREMENT,
+        _url_join,
+    )
+
+from playwright.sync_api import sync_playwright
+
+BASE_URL = "http://127.0.0.1:8080"
+EXPECTED_TITLE = "PyScript Hello World"
+HTML_FILE = "hello_world.html"
+
+
+def test():
+    """Wrapper for all tests for this example so as to use one playwright instance"""
+
+    url = _url_join(BASE_URL, HTML_FILE)
+
+    # Set up playwright package
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto(url)
+
+        # Step 1: Test page title to ensure HTML is loaded properly
+
+        print(f"{HTML_FILE}: Asserting <title></title> == {EXPECTED_TITLE}")
+        assert page.title() == EXPECTED_TITLE
+
+        # Step 2: Test that pyodide is loaded via the console
+
+        loading_messages = [
+            "Loading runtime...",
+            "Runtime created...",
+            "Initializing components...",
+            "Initializing scripts...",
+        ]
+        rng = math.ceil(MAX_TEST_TIME / TEST_TIME_INCREMENT)  # 30 / 0.25 = 120 iters
+        pyodide_loading = False
+
+        for _ in range(rng):
+            time.sleep(TEST_TIME_INCREMENT)
+            content = page.text_content("*")
+            for msg in loading_messages:
+                if msg in content:
+                    pyodide_loading = True
+            if pyodide_loading:
+                break
+
+        print(f"{HTML_FILE}: Asserting that pyodide is loading")
+        assert pyodide_loading
+
+        # Step 3:
+        # Assert that rendering inserts data into the page as expected
+        # (In other words, search DOM for data not in initial markup
+        # but present after rendering)
+
+        re_sub_content = re.compile(r"\d+/\d+/\d+, \d+:\d+:\d+")  # Timestamp
+
+        py_rendered = False
+        for _ in range(rng):
+            time.sleep(TEST_TIME_INCREMENT)
+            content = page.inner_html("*")
+            if re_sub_content.search(content):
+                py_rendered = True
+                break
+
+        print(f"{HTML_FILE}: Asserting that example rendered")
+        assert py_rendered
+
+
+if __name__ == "__main__":
+    test()
+    print(f"{HTML_FILE}: Finished")
+    print()
