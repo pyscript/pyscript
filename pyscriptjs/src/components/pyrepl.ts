5b1e4587334708af75b679d7bf3c5e3a1dccb2d0
Mariano Weber <info@uiremotely.com>
2022-05-14 10:57:54 +0200

    fix: REPL play button alignment

--- pyscriptjs/src/components/pyrepl.ts
+++ pyscriptjs/src/components/pyrepl.ts
@@ -100,8 +100,8 @@ export class PyRepl extends BaseEvalElement {
         // Play Button
         this.btnRun = document.createElement('button');
         this.btnRun.innerHTML =
-            '<svg id="" class="svelte-fa svelte-ps5qeg" style="height:20px;width:20px;vertical-align:-.125em;transform-origin:center;overflow:visible;color:green" viewBox="0 0 384 512" aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg"><g transform="translate(192 256)" transform-origin="96 0"><g transform="translate(0,0) scale(1,1)"><path d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z" fill="currentColor" transform="translate(-192 -256)"></path></g></g></svg>';
-        addClasses(this.btnRun, ['absolute', 'right-1', 'bottom-3', 'opacity-0', 'group-hover:opacity-100']);
+            '<svg id="" class="svelte-fa svelte-ps5qeg" style="height:20px;width:20px;color:green" viewBox="0 0 384 512" aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg"><g transform="translate(192 256)" transform-origin="96 0"><g transform="translate(0,0) scale(1,1)"><path d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z" fill="currentColor" transform="translate(-192 -256)"></path></g></g></svg>';
+        addClasses(this.btnRun, ['absolute', 'right-1', 'bottom-0', 'top-0', 'opacity-0', 'group-hover:opacity-100']);
         this.editorNode.appendChild(this.btnRun);
 
         this.btnRun.onclick = wrap(this);
@@ -176,6 +176,7 @@ export class PyRepl extends BaseEvalElement {
 
             newPyRepl.setAttribute('root', this.getAttribute('root'));
             newPyRepl.id = this.getAttribute('root') + '-' + nextExecId.toString();
+            console.log(`NEW ID = ${newPyRepl.id}`)
             newPyRepl.setAttribute('auto-generate', null);
 
             if (this.hasAttribute('output')) {
