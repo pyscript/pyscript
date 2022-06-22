f5a2442bfc4115f36e57083baba5a9fffbf284df
woxtu <woxtup@gmail.com>
2022-06-22 21:34:46 +0900

    Import the type definition

--- pyscriptjs/src/components/pyscript.ts
+++ pyscriptjs/src/components/pyscript.ts
@@ -8,6 +8,7 @@ import {
 } from '../stores';
 import { addClasses, htmlDecode } from '../utils';
 import { BaseEvalElement } from './base';
+import type { PyodideInterface } from '../pyodide';
 
 // Premise used to connect to the first available pyodide interpreter
 let pyodideReadyPromise;
@@ -24,11 +25,6 @@ loadedEnvironments.subscribe(value => {
 mode.subscribe(value => {
     currentMode = value;
 });
-
-// TODO: use type declaractions
-type PyodideInterface = {
-    registerJsModule(name: string, module: object): void;
-};
 
 export class PyScript extends BaseEvalElement {
     constructor() {
