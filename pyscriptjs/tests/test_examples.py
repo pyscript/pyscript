da1475e21e636504a59bc0fff9f10a3608453b0e
Antonio Cuni <anto.cuni@gmail.com>
2022-08-09 12:21:23 +0200

    undo this modification. I don't know exactly how it is possible, but for some examples it seems that PyodideRuntime.initialize is not called and thus the magic message is never prited to the console. We probably need to investigate more in another PR

--- pyscriptjs/tests/test_examples.py
+++ pyscriptjs/tests/test_examples.py
@@ -155,10 +155,18 @@ def wait_for_load(page):
     """
     Assert that pyscript loading messages appear.
     """
-    # this is printed by pyconfig.ts:PyodideRuntime.initialize
-    magic = "===PyScript page fully initialized==="
-    with page.expect_console_message(lambda m: m.text == magic):
-        pass
+    pyodide_loading = False  # Flag to be set to True when condition met
+
+    for _ in range(TEST_ITERATIONS):
+        content = page.text_content("*")
+        for message in LOADING_MESSAGES:
+            if message in content:
+                pyodide_loading = True
+        if pyodide_loading:
+            break
+        time.sleep(TEST_TIME_INCREMENT)
+
+    assert pyodide_loading  # nosec
 
 
 def wait_for_render(page, selector, pattern):
