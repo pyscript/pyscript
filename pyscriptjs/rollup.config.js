17187ba3eccf4363a583c76e65f784e8e61976f7
woxtu <woxtup@gmail.com>
2022-08-27 06:10:37 +0900

    Remove the Svelte plugins for Rollup (#730)

--- pyscriptjs/rollup.config.js
+++ pyscriptjs/rollup.config.js
@@ -1,9 +1,7 @@
-import svelte from "rollup-plugin-svelte";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
-import sveltePreprocess from "svelte-preprocess";
 import typescript from "@rollup/plugin-typescript";
 import css from "rollup-plugin-css-only";
 import serve from "rollup-plugin-serve";
@@ -31,17 +29,6 @@ export default {
     },
   ],
   plugins: [
-    svelte({
-      // add postcss config
-      preprocess: sveltePreprocess({
-        postcss: {
-          plugins: [require("autoprefixer")],
-        },
-      }),
-      compilerOptions: {
-        dev: !production,
-      },
-    }),
     css({ output: "pyscript.css" }),
     // Bundle all the Python files into the output file
     string({
