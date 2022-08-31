d9ad15682389763ef22d11aaebbf6dd27a5108a6
Madhur Tandon <madhurtandon23@gmail.com>
2022-08-31 19:40:17 +0530

    check that py-config loads an older version

--- pyscriptjs/tests/integration/test_py_config.py
+++ pyscriptjs/tests/integration/test_py_config.py
@@ -1,18 +1,31 @@
-import re
+import tarfile
+from io import BytesIO
 
-from .support import ROOT, PyScriptTest
+import requests
 
-PYODIDE = ROOT.join("pyscriptjs", "node_modules", "pyodide")
+from .support import PyScriptTest
+
+URL = "https://github.com/pyodide/pyodide/releases/download/0.20.0/pyodide-build-0.20.0.tar.bz2"
+
+
+def download_and_unzip(url, extract_to="."):
+    response = requests.get(url, stream=True)
+    file = tarfile.open(fileobj=BytesIO(response.raw.read()), mode="r:bz2")
+    file.extractall(path=extract_to)
 
 
 class TestPyConfig(PyScriptTest):
     def test_config(self):
-        self.tmpdir.join("pyodide").mksymlinkto(PYODIDE)
+        download_and_unzip(
+            url=URL,
+            extract_to=self.tmpdir,
+        )
 
         self.pyscript_run(
             snippet="""
             <py-script>
-                print('hello pyscript')
+                import sys
+                sys.modules["pyodide"].__version__
             </py-script>
         """,
             config="""
@@ -20,12 +33,11 @@ class TestPyConfig(PyScriptTest):
                 autoclose_loader: true
                 runtimes:
                 - src: "/pyodide/pyodide.js"
-                  name: pyodide-0.21
+                  name: pyodide-0.20.0
                   lang: python
             </py-config>
         """,
         )
 
-        inner_html = self.page.locator("py-script").inner_html()
-        pattern = r'<div id="py-.*">hello pyscript</div>'
-        assert re.search(pattern, inner_html)
+        version = self.page.locator("py-script").inner_text()
+        assert version == "0.20.0"
