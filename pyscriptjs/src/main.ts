77be53a67037b5a02ceae06703e00ddd513b4a2a
Antonio Cuni <anto.cuni@gmail.com>
2022-09-29 13:47:05 +0200

    start to remove module-level statements, and put all the logic in its own function

--- pyscriptjs/src/main.ts
+++ pyscriptjs/src/main.ts
@@ -9,23 +9,30 @@ import { globalLoader } from './stores';
 
 const logger = getLogger('pyscript/main');
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
-const xPyScript = customElements.define('py-script', PyScript);
-const xPyLoader = customElements.define('py-loader', PyLoader);
-const xPyConfig = customElements.define('py-config', PyConfig);
-const xPyEnv = customElements.define('py-env', PyEnv);
-/* eslint-disable @typescript-eslint/no-unused-vars */
+function pyscript_main() {
+    /* eslint-disable @typescript-eslint/no-unused-vars */
+    const xPyScript = customElements.define('py-script', PyScript);
+    const xPyLoader = customElements.define('py-loader', PyLoader);
+    const xPyConfig = customElements.define('py-config', PyConfig);
+    const xPyEnv = customElements.define('py-env', PyEnv);
+    /* eslint-disable @typescript-eslint/no-unused-vars */
 
-// As first thing, loop for application configs
-logger.info('checking for py-config');
-const config: PyConfig = document.querySelector('py-config');
-if (!config) {
-    const loader = document.createElement('py-config');
+    // As first thing, loop for application configs
+    logger.info('checking for py-config');
+    const config: PyConfig = document.querySelector('py-config');
+    if (!config) {
+        const loader = document.createElement('py-config');
+        document.body.append(loader);
+    }
+
+    // add loader to the page body
+    logger.info('add py-loader');
+    const loader = <PyLoader>document.createElement('py-loader');
     document.body.append(loader);
+    globalLoader.set(loader);
+
+
 }
 
-// add loader to the page body
-logger.info('add py-loader');
-const loader = <PyLoader>document.createElement('py-loader');
-document.body.append(loader);
-globalLoader.set(loader);
+// main entry point of execution
+pyscript_main();
