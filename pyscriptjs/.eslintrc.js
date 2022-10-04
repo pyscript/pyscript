80ae6de246b871490d952fb339d31369473104c1
Ted Patrick <tpatrick@anaconda.com>
2022-10-03 21:31:19 -0500

    full eslint

--- pyscriptjs/.eslintrc.js
+++ pyscriptjs/.eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
     rules: {
         'no-prototype-builtins': 'warn',
         '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/no-explicit-any': 'warn',
         '@typescript-eslint/no-unsafe-assignment': 'warn',
         '@typescript-eslint/no-unsafe-argument': 'warn',
         '@typescript-eslint/no-unsafe-member-access': 'warn',
