af50a9ab981299765242fa85e37a911859c3e6ec
Antonio Cuni <anto.cuni@gmail.com>
2022-10-13 05:16:08 +0200

    while I am at it, kill all these svelte stores which are unused

--- pyscriptjs/src/stores.ts
+++ pyscriptjs/src/stores.ts
@@ -5,14 +5,7 @@ import type { Runtime } from './runtime';
 import type { AppConfig } from './pyconfig';
 import { getLogger } from './logger';
 
-
-export const navBarOpen = writable(false);
-export const componentsNavOpen = writable(false);
-export const componentDetailsNavOpen = writable(false);
-export const mainDiv = writable(null);
-export const currentComponentDetails = writable([]);
 export const scriptsQueue = writable<PyScript[]>([]);
-export const globalLoader = writable<PyLoader | undefined>();
 
 export const addToScriptsQueue = (script: PyScript) => {
     scriptsQueue.update(scriptsQueue => [...scriptsQueue, script]);
