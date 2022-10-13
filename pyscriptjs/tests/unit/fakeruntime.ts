080cb9eda4918dc4ef05800bef85818b5c74f36e
Antonio Cuni <anto.cuni@gmail.com>
2022-10-13 11:16:38 +0200

    fix pyinputbox.test.ts

---
+++ pyscriptjs/tests/unit/fakeruntime.ts
@@ -0,0 +1,40 @@
+import { Runtime } from "../../src/runtime"
+import type { PyodideInterface } from 'pyodide';
+
+export class FakeRuntime extends Runtime {
+
+    src: string;
+    name?: string;
+    lang?: string;
+    interpreter: PyodideInterface;
+    globals: any;
+
+    constructor() {
+        super(null);
+    }
+
+    async run(code: string) {
+        /* don't to anything */
+    }
+
+    async loadInterpreter() {
+        throw new Error("not implemented");
+    }
+
+
+    registerJsModule(name: string, module: object) {
+        throw new Error("not implemented");
+    }
+
+    async loadPackage(names: string | string[]) {
+        throw new Error("not implemented");
+    }
+
+    async installPackage(package_name: string | string[]) {
+        throw new Error("not implemented");
+    }
+
+    async loadFromFile(path: string) {
+        throw new Error("not implemented");
+    }
+}
