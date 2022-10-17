7ddfb3855320afa1202c15166372a0b47544fab0
Antonio Cuni <anto.cuni@gmail.com>
2022-10-17 10:20:30 +0200

    kill unused imports

--- pyscriptjs/src/stores.ts
+++ pyscriptjs/src/stores.ts
@@ -1,9 +1,5 @@
 import { writable } from 'svelte/store';
-import type { PyLoader } from './components/pyloader';
 import type { PyScript } from './components/pyscript';
-import type { Runtime } from './runtime';
-import type { AppConfig } from './pyconfig';
-import { getLogger } from './logger';
 
 export const scriptsQueue = writable<PyScript[]>([]);
 
