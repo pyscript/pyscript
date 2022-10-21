507466a704fccebffda73ef2cf1f3d04ab63ac1f
Antonio Cuni <anto.cuni@gmail.com>
2022-10-21 17:29:10 +0200

    remove the <string> cast, it's not necessary

--- pyscriptjs/src/pyexec.ts
+++ pyscriptjs/src/pyexec.ts
@@ -5,10 +5,10 @@ type OutputMode = "append" | "replace";
 export async function pyExec(runtime: Runtime, pysrc: string, targetID: string)
 {
     try {
-        <string>await runtime.run(`set_current_display_target(target_id="${targetID}")`);
-        <string>await runtime.run(pysrc);
+        await runtime.run(`set_current_display_target(target_id="${targetID}")`);
+        await runtime.run(pysrc);
     }
     finally {
-        <string>await runtime.run(`set_current_display_target(target_id=None)`);
+        await runtime.run(`set_current_display_target(target_id=None)`);
     }
 }
