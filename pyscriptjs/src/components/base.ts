ecac29ad0298c67aad7eaf7a89551894f3a51819
Antonio Cuni <anto.cuni@gmail.com>
2022-10-23 00:51:55 +0200

    add a comment

--- pyscriptjs/src/components/base.ts
+++ pyscriptjs/src/components/base.ts
@@ -1,3 +1,8 @@
+// XXX this should be eventually killed.
+// The only remaining class which inherit from BaseEvalElement is PyRepl: we
+// should merge the two classes together, do a refactoing of how PyRepl to use
+// the new pyExec and in general clean up the unnecessary code.
+
 import { ensureUniqueId, addClasses, removeClasses, getAttribute } from '../utils';
 import type { Runtime } from '../runtime';
 import { getLogger } from '../logger';
