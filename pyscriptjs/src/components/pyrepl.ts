111e8dfe9b08c8e38397a6794d78f2b5e068a031
Antonio Cuni <anto.cuni@gmail.com>
2022-10-25 18:50:25 +0200

    move code around so that the order in the code matches the order of execution

--- pyscriptjs/src/components/pyrepl.ts
+++ pyscriptjs/src/components/pyrepl.ts
@@ -97,25 +97,35 @@ export function make_PyRepl(runtime: Runtime) {
             });
         }
 
+        // ******** main entry point for py-repl DOM building **********
+        //
+        // The following functions are written in a top-down, depth-first
+        // order (so that the order of code roughly matches the order of
+        // execution)
+        makeBoxDiv(): HTMLElement {
+            const boxDiv = document.createElement('div');
+            boxDiv.className = 'py-repl-box';
+
+            const editorDiv = this.makeEditorDiv();
+            const editorLabel = this.makeLabel('Python Script Area', editorDiv);
+            boxDiv.append(editorLabel);
+            boxDiv.appendChild(editorDiv);
+
+            return boxDiv;
+        }
+
         makeEditorDiv(): HTMLElement {
             const editorDiv = document.createElement('div');
             editorDiv.id = 'code-editor';
             editorDiv.className = 'py-repl-editor';
             editorDiv.appendChild(this.editor.dom);
-            return editorDiv;
-        }
 
-        makeRunButton(): HTMLElement {
-            const runButton = document.createElement('button');
-            runButton.id = 'runButton';
-            runButton.className = 'absolute py-repl-run-button';
-            // XXX I'm sure there is a better way to embed svn into typescript
-            runButton.innerHTML =
-                '<svg id="" style="height:20px;width:20px;vertical-align:-.125em;transform-origin:center;overflow:visible;color:green" viewBox="0 0 384 512" aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg"><g transform="translate(192 256)" transform-origin="96 0"><g transform="translate(0,0) scale(1,1)"><path d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z" fill="currentColor" transform="translate(-192 -256)"></path></g></g></svg>';
-            runButton.addEventListener('click', () => {
-                void this.evaluate(runtime);
-            });
-            return runButton;
+            const runButton = this.makeRunButton();
+            const runLabel = this.makeLabel('Python Script Run Button', runButton);
+            editorDiv.appendChild(runLabel);
+            editorDiv.appendChild(runButton);
+
+            return editorDiv;
         }
 
         makeLabel(text: string, elementFor: HTMLElement): HTMLElement {
@@ -130,21 +140,17 @@ export function make_PyRepl(runtime: Runtime) {
             return lbl;
         }
 
-        makeBoxDiv(): HTMLElement {
-            const boxDiv = document.createElement('div');
-            boxDiv.className = 'py-repl-box';
-
-            const editorDiv = this.makeEditorDiv();
-            const editorLabel = this.makeLabel('Python Script Area', editorDiv);
-            boxDiv.append(editorLabel);
-            boxDiv.appendChild(editorDiv);
-
-            const runButton = this.makeRunButton();
-            const runLabel = this.makeLabel('Python Script Run Button', runButton);
-
-            editorDiv.appendChild(runLabel);
-            editorDiv.appendChild(runButton);
-            return boxDiv;
+        makeRunButton(): HTMLElement {
+            const runButton = document.createElement('button');
+            runButton.id = 'runButton';
+            runButton.className = 'absolute py-repl-run-button';
+            // XXX I'm sure there is a better way to embed svn into typescript
+            runButton.innerHTML =
+                '<svg id="" style="height:20px;width:20px;vertical-align:-.125em;transform-origin:center;overflow:visible;color:green" viewBox="0 0 384 512" aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg"><g transform="translate(192 256)" transform-origin="96 0"><g transform="translate(0,0) scale(1,1)"><path d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z" fill="currentColor" transform="translate(-192 -256)"></path></g></g></svg>';
+            runButton.addEventListener('click', () => {
+                void this.evaluate(runtime);
+            });
+            return runButton;
         }
 
         makeOutDiv(): HTMLElement {
