pyscript/pyscript

commit 214e39537bf18e1bec65153fdaa2fce355999693

Simplify pyrepl.ts and kill base.ts (#884)
Major highlights:

1. Merge&simplify base.ts and pyrepl.ts; kill base.ts
2. improve and extente the py-repl integration tests
3. Reorder the code in pyrepl.ts. This part of the PR doesn't change much of the concrete logic: it's just a sequence of renaming variables, moving code around, group code into functions, killing code which is no longer needed. But the end result is much better and nicer to read, IMHO.

Minor highlights:

1. py-repl now uses the new logic in pyexec.ts to run the code

2. after PR Add display impl, rm outputManage, print and console.log default to browser console #749 py-repl no longer displayed the result of the last evaluated expression (e.g. if you typed 42 and run it, it displayed nothing). This PR re-introduces this behavior, which is what you would expect by a REPL.

3. improve the pytest --dev option: now it implies --no-fake-server so that sourcemaps works automatically

4. improve the names of the CSS classes to be more consistent

5. kill pyrepl.test.ts: the old tests didn't check anything useful,  this style of unit test doesn't really add much value if you have good integration tests (which now we have) and trying to revive them was not worth the hassle

pyscriptjs/src/components/pyrepl.ts
@@ -5,161 +5,201 @@ import { Compartment, StateCommand } from '@codemirror/state';
 import { keymap } from '@codemirror/view';
 import { defaultKeymap } from '@codemirror/commands';
 import { oneDarkTheme } from '@codemirror/theme-one-dark';
-import { getAttribute, addClasses, htmlDecode } from '../utils';
-import { BaseEvalElement } from './base';
+
+import { getAttribute,  ensureUniqueId, htmlDecode } from '../utils';
 import type { Runtime } from '../runtime';
+import { pyExec, pyDisplay } from '../pyexec';
 import { getLogger } from '../logger';
 
 const logger = getLogger('py-repl');
 
 export function make_PyRepl(runtime: Runtime) {
 
-    function createCmdHandler(el: PyRepl): StateCommand {
-        // Creates a codemirror cmd handler that calls the el.evaluate when an event
-        // triggers that specific cmd
-        return () => {
-            void el.evaluate(runtime);
-            return true;
-        };
-    }
+    /* High level structure of py-repl DOM, and the corresponding JS names.
 
-    let initialTheme: string;
-    function getEditorTheme(el: BaseEvalElement): string {
-        const theme = getAttribute(el, 'theme');
-        if( !initialTheme && theme){
-            initialTheme = theme;
-        }
-        return initialTheme;
-    }
-
-    class PyRepl extends BaseEvalElement {
+           this             <py-repl>
+           shadow               #shadow-root
+                                    <slot></slot>
+           boxDiv               <div class='py-repl-box'>
+           editorLabel              <label>...</label>
+           editorDiv                <div class="py-repl-editor"></div>
+           outDiv                   <div class="py-repl-output"></div>
+                                </div>
+                            </py-repl>
+    */
+    class PyRepl extends HTMLElement {
+        shadow: ShadowRoot;
+        outDiv: HTMLElement;
         editor: EditorView;
-        editorNode: HTMLElement;
 
         constructor() {
             super();
-
-            // add an extra div where we can attach the codemirror editor
-            this.editorNode = document.createElement('div');
-            addClasses(this.editorNode, ['editor-box']);
-            this.shadow.appendChild(this.wrapper);
         }
 
         connectedCallback() {
-            this.checkId();
-            this.code = htmlDecode(this.innerHTML);
-            this.innerHTML = '';
-            const languageConf = new Compartment();
+            ensureUniqueId(this);
+            this.shadow = this.attachShadow({ mode: 'open' });
+            const slot = document.createElement('slot');
+            this.shadow.appendChild(slot);
 
+            if (!this.hasAttribute('exec-id')) {
+                this.setAttribute('exec-id', '1');
+            }
+            if (!this.hasAttribute('root')) {
+                this.setAttribute('root', this.id);
+            }
+
+            const pySrc = htmlDecode(this.innerHTML).trim();
+            this.innerHTML = '';
+            this.editor = this.makeEditor(pySrc);
+            const boxDiv = this.makeBoxDiv();
+            this.appendChild(boxDiv);
+            this.editor.focus();
+            logger.debug(`element ${this.id} successfully connected`);
+        }
+
+        /** Create and configure the codemirror editor
+         */
+        makeEditor(pySrc: string): EditorView {
+            const languageConf = new Compartment();
             const extensions = [
                 indentUnit.of("    "),
                 basicSetup,
                 languageConf.of(python()),
                 keymap.of([
                     ...defaultKeymap,
-                    { key: 'Ctrl-Enter', run: createCmdHandler(this) },
-                    { key: 'Shift-Enter', run: createCmdHandler(this) },
+                    { key: 'Ctrl-Enter',  run: this.execute.bind(this) },
+                    { key: 'Shift-Enter', run: this.execute.bind(this) },
                 ]),
             ];
 
-            if (getEditorTheme(this) === 'dark') {
+            if (getAttribute(this, 'theme') === 'dark') {
                 extensions.push(oneDarkTheme);
             }
 
-            this.editor = new EditorView({
-                doc: this.code.trim(),
+            return new EditorView({
+                doc: pySrc,
                 extensions,
-                parent: this.editorNode,
             });
+        }
 
-            const mainDiv = document.createElement('div');
-            addClasses(mainDiv, ['py-repl-box']);
+        // ******** main entry point for py-repl DOM building **********
+        //
+        // The following functions are written in a top-down, depth-first
+        // order (so that the order of code roughly matches the order of
+        // execution)
+        makeBoxDiv(): HTMLElement {
+            const boxDiv = document.createElement('div');
+            boxDiv.className = 'py-repl-box';
 
+            const editorDiv = this.makeEditorDiv();
+            const editorLabel = this.makeLabel('Python Script Area', editorDiv);
+            this.outDiv = this.makeOutDiv();
+
+            boxDiv.append(editorLabel);
+            boxDiv.appendChild(editorDiv);
+            boxDiv.appendChild(this.outDiv);
+
+            return boxDiv;
+        }
+
+        makeEditorDiv(): HTMLElement {
+            const editorDiv = document.createElement('div');
+            editorDiv.id = 'code-editor';
+            editorDiv.className = 'py-repl-editor';
+            editorDiv.appendChild(this.editor.dom);
+
+            const runButton = this.makeRunButton();
+            const runLabel = this.makeLabel('Python Script Run Button', runButton);
+            editorDiv.appendChild(runLabel);
+            editorDiv.appendChild(runButton);
+
+            return editorDiv;
+        }
+
+        makeLabel(text: string, elementFor: HTMLElement): HTMLElement {
+            ensureUniqueId(elementFor);
+            const lbl = document.createElement('label');
+            lbl.innerHTML = text;
+            lbl.htmlFor = elementFor.id;
+            // XXX this should be a CSS class
             // Styles that we use to hide the labels whilst also keeping it accessible for screen readers
             const labelStyle = 'overflow:hidden; display:block; width:1px; height:1px';
+            lbl.setAttribute('style', labelStyle);
+            return lbl;
+        }
 
-            // Code editor Label
-            this.editorNode.id = 'code-editor';
-            const editorLabel = document.createElement('label');
-            editorLabel.innerHTML = 'Python Script Area';
-            editorLabel.setAttribute('style', labelStyle);
-            editorLabel.htmlFor = 'code-editor';
-
-            mainDiv.append(editorLabel);
-
-            // add Editor to main PyScript div
-            mainDiv.appendChild(this.editorNode);
-
-            // Play Button
-            this.btnRun = document.createElement('button');
-            this.btnRun.id = 'btnRun';
-            this.btnRun.innerHTML =
+        makeRunButton(): HTMLElement {
+            const runButton = document.createElement('button');
+            runButton.id = 'runButton';
+            runButton.className = 'absolute py-repl-run-button';
+            // XXX I'm sure there is a better way to embed svn into typescript
+            runButton.innerHTML =
                 '<svg id="" style="height:20px;width:20px;vertical-align:-.125em;transform-origin:center;overflow:visible;color:green" viewBox="0 0 384 512" aria-hidden="true" role="img" xmlns="http://www.w3.org/2000/svg"><g transform="translate(192 256)" transform-origin="96 0"><g transform="translate(0,0) scale(1,1)"><path d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z" fill="currentColor" transform="translate(-192 -256)"></path></g></g></svg>';
-            addClasses(this.btnRun, ['absolute', 'repl-play-button']);
+            runButton.addEventListener('click', this.execute.bind(this));
+            return runButton;
+        }
 
-            // Play Button Label
-            const btnLabel = document.createElement('label');
-            btnLabel.innerHTML = 'Python Script Run Button';
-            btnLabel.setAttribute('style', labelStyle);
-            btnLabel.htmlFor = 'btnRun';
+        makeOutDiv(): HTMLElement {
+            const outDiv = document.createElement('div');
+            outDiv.className = 'py-repl-output';
+            outDiv.id = this.id + '-' + this.getAttribute('exec-id');
+            return outDiv;
+        }
 
-            this.editorNode.appendChild(btnLabel);
-            this.editorNode.appendChild(this.btnRun);
+        //  ********************* execution logic *********************
 
-            this.btnRun.addEventListener('click', () => {
-                void this.evaluate(runtime);
-            });
+        /** Execute the python code written in the editor, and automatically
+         *  display() the last evaluated expression
+         */
+        async execute(): Promise<void> {
+            const pySrc = this.getPySrc();
 
-            if (!this.id) {
-                logger.warn(
-                    "WARNING: <py-repl> defined without an id. <py-repl> should always have an id, otherwise multiple <py-repl> in the same page will not work!"
-                );
+            // determine the output element
+            const outEl = this.getOutputElement();
+            if (outEl === undefined) {
+                // this happens if we specified output="..." but we couldn't
+                // find the ID. We already displayed an error message inside
+                // getOutputElement, stop the execution.
+                return;
             }
 
-            if (!this.hasAttribute('exec-id')) {
-                this.setAttribute('exec-id', '1');
-            }
+            // clear the old output before executing the new code
+            outEl.innerHTML = '';
 
-            if (!this.hasAttribute('root')) {
-                this.setAttribute('root', this.id);
-            }
+            // execute the python code
+            const pyResult = await pyExec(runtime, pySrc, outEl);
 
-            const output = getAttribute(this, "output")
-            if (output) {
-                const el = document.getElementById(output);
-                if(el){
-                    this.errorElement = el;
-                    this.outputElement = el
+            // display the value of the last evaluated expression (REPL-style)
+            if (pyResult !== undefined) {
+                pyDisplay(runtime, pyResult, { target: outEl.id });
+            }
+        }
+
+        getPySrc(): string {
+            return this.editor.state.doc.toString();
+        }
+
+        getOutputElement(): HTMLElement {
+            const outputID = getAttribute(this, "output");
+            if (outputID !== null) {
+                const el = document.getElementById(outputID);
+                if (el === null) {
+                    const err = `py-repl ERROR: cannot find the output element #${outputID} in the DOM`
+                    this.outDiv.innerText = err;
+                    return undefined;
                 }
-            } else {
-                // to create a new output div to output to
-                this.outputElement = document.createElement('div');
-                this.outputElement.classList.add('output');
-                this.outputElement.hidden = true;
-                this.outputElement.id = this.id + '-' + this.getAttribute('exec-id');
-
-                // add the output div id if there's not output pre-defined
-                mainDiv.appendChild(this.outputElement);
-
-                this.errorElement = this.outputElement;
+                return el;
             }
-
-            this.appendChild(mainDiv);
-            this.editor.focus();
-            logger.debug(`element ${this.id} successfully connected`);
-        }
-
-        preEvaluate(): void {
-            this.setOutputMode("replace");
-            if(!this.appendOutput) {
-                this.outputElement.innerHTML = '';
+            else {
+                return this.outDiv;
             }
         }
 
-        postEvaluate(): void {
-            this.outputElement.hidden = false;
-            this.outputElement.style.display = 'block';
 
+        // XXX the autogenerate logic is very messy. We should redo it, and it
+        // should be the default.
+        autogenerateMaybe(): void {
             if (this.hasAttribute('auto-generate')) {
                 const allPyRepls = document.querySelectorAll(`py-repl[root='${this.getAttribute('root')}'][exec-id]`);
                 const lastRepl = allPyRepls[allPyRepls.length - 1];
@@ -196,9 +236,6 @@ export function make_PyRepl(runtime: Runtime) {
             }
         }
 
-        getSourceFromElement(): string {
-            return this.editor.state.doc.toString();
-        }
     }
 
     return PyRepl

pyscriptjs/tests/integration/test_py_repl.py
@@ -1,115 +1,231 @@
-import pytest
-from playwright.sync_api import expect
-
 from .support import PyScriptTest
 
 
 class TestPyRepl(PyScriptTest):
+    def _replace(self, py_repl, newcode):
+        """
+        Clear the editor and write new code in it.
+        WARNING: this assumes that the textbox has already the focus
+        """
+        # clear the editor, write new code
+        self.page.keyboard.press("Control+A")
+        self.page.keyboard.press("Backspace")
+        self.page.keyboard.type(newcode)
+
     def test_repl_loads(self):
         self.pyscript_run(
             """
-            <py-repl id="my-repl" auto-generate="true"> </py-repl>
+            <py-repl></py-repl>
             """
         )
-
         py_repl = self.page.query_selector("py-repl")
         assert py_repl
         assert "Python" in py_repl.inner_text()
 
-    def test_repl_runs_on_button_press(self):
+    def test_execute_preloaded_source(self):
+        """
+        Unfortunately it tests two things at once, but it's impossible to write a
+        smaller test. I think this is the most basic test that we can write.
+
+        We test that:
+            1. the source code that we put in the tag is loaded inside the editor
+            2. clicking the button executes it
+        """
         self.pyscript_run(
             """
-            <py-repl id="my-repl" auto-generate="true"> </py-repl>
+            <py-repl>
+                print('hello from py-repl')
+            </py-repl>
             """
         )
+        py_repl = self.page.locator("py-repl")
+        src = py_repl.inner_text()
+        assert "print('hello from py-repl')" in src
+        py_repl.locator("button").click()
+        assert self.console.log.lines[-1] == "hello from py-repl"
 
-        self.page.locator("py-repl").type('display("hello")')
-
-        # We only have one button in the page
-        self.page.locator("button").click()
-
-        # The result gets the id of the repl + n
-        repl_result = self.page.wait_for_selector("#my-repl-2", state="attached")
-
-        assert repl_result.inner_text() == "hello"
-
-    def test_repl_runs_with_shift_enter(self):
+    def test_execute_code_typed_by_the_user(self):
         self.pyscript_run(
             """
-            <py-repl id="my-repl" auto-generate="true"> </py-repl>
+            <py-repl></py-repl>
             """
         )
-        self.page.locator("py-repl").type('display("hello")')
+        py_repl = self.page.locator("py-repl")
+        py_repl.type('print("hello")')
+        py_repl.locator("button").click()
+        assert self.console.log.lines[-1] == "hello"
 
-        # Confirm that we get a result by using the keys shortcut
+    def test_execute_on_shift_enter(self):
+        self.pyscript_run(
+            """
+            <py-repl>
+                print("hello world")
+            </py-repl>
+            """
+        )
         self.page.keyboard.press("Shift+Enter")
-        py_repl = self.page.query_selector("#my-repl-2")
-        assert "hello" in py_repl.inner_text()
 
-    def test_repl_console_ouput(self):
+        # when we use locator('button').click() the message appears
+        # immediately, with keyboard.press we need to wait for it. I don't
+        # really know why it has a different behavior, I didn't investigate
+        # further.
+        self.wait_for_console("hello world")
+
+    def test_display(self):
         self.pyscript_run(
             """
-            <py-repl id="my-repl" auto-generate="true"> </py-repl>
+            <py-repl>
+                display('hello world')
+            </py-repl>
             """
         )
-        self.page.locator("py-repl").type("print('apple')")
-        self.page.keyboard.press("Enter")
-        self.page.locator("py-repl").type("console.log('banana')")
-        self.page.locator("button").click()
+        py_repl = self.page.locator("py-repl")
+        py_repl.locator("button").click()
+        out_div = py_repl.locator("div.py-repl-output")
+        assert out_div.inner_text() == "hello world"
 
-        # The result gets the id of the repl + n
-        repl_result = self.page.wait_for_selector("#my-repl-1", state="attached")
-
-        assert repl_result.inner_text() == ""
-
-    def test_repl_error_ouput(self):
+    def test_show_last_expression(self):
+        """
+        Test that we display() the value of the last expression, as you would
+        expect by a REPL
+        """
         self.pyscript_run(
             """
-            <py-repl id="my-repl" auto-generate="true"> </py-repl>
+            <py-repl>
+                42
+            </py-repl>
             """
         )
-        self.page.locator("py-repl").type("this is an error")
-        self.page.locator("button").click()
-        expect(self.page.locator(".py-error")).to_be_visible()
+        py_repl = self.page.locator("py-repl")
+        py_repl.locator("button").click()
+        out_div = py_repl.locator("div.py-repl-output")
+        assert out_div.inner_text() == "42"
 
-    # console errors are observable on the headed instance
-    # but is just not possible to access them using the self object
-    @pytest.mark.xfail(reason="Cannot access console errors")
-    def test_repl_error_ouput_console(self):
+    def test_run_clears_previous_output(self):
+        """
+        Check that we clear the previous output of the cell before executing it
+        again
+        """
         self.pyscript_run(
             """
-            <py-repl id="my-repl" auto-generate="true"> </py-repl>
+            <py-repl>
+                display('hello world')
+            </py-repl>
             """
         )
-        self.page.locator("py-repl").type("this is an error")
-        self.page.locator("button").click()
-
-    def test_repl_error_and_fail_moving_forward_ouput(self):
-        self.pyscript_run(
-            """
-            <py-repl id="my-repl" auto-generate="true"> </py-repl>
-            """
-        )
-        self.page.locator("py-repl").type("this is an error")
-        self.page.locator("button").click()
-        expect(self.page.locator(".py-error")).to_be_visible()
+        py_repl = self.page.locator("py-repl")
+        out_div = py_repl.locator("div.py-repl-output")
         self.page.keyboard.press("Shift+Enter")
-        expect(self.page.locator(".py-error")).to_be_visible()
+        assert out_div.inner_text() == "hello world"
+        #
+        # clear the editor, write new code, execute
+        self._replace(py_repl, "display('another output')")
+        self.page.keyboard.press("Shift+Enter")
+        assert out_div.inner_text() == "another output"
 
-    # this tests the fact that a new error div should be created once there's
-    # an error but also that it should disappear automatically once the error
-    # is fixed
-    def test_repl_show_error_fix_error_check_for_ouput(self):
+    def test_python_exception(self):
+        """
+        See also test01_basic::test_python_exception, since it's very similar
+        """
         self.pyscript_run(
             """
-            <py-repl id="my-repl" auto-generate="true"> </py-repl>
+            <py-repl>
+                raise Exception('this is an error')
+            </py-repl>
             """
         )
-        self.page.locator("py-repl").type("d")
+        py_repl = self.page.locator("py-repl")
+        py_repl.locator("button").click()
+        #
+        # check that we sent the traceback to the console
+        tb_lines = self.console.error.lines[-1].splitlines()
+        assert tb_lines[0] == "[pyexec] Python exception:"
+        assert tb_lines[1] == "Traceback (most recent call last):"
+        assert tb_lines[-1] == "Exception: this is an error"
+        #
+        # check that we show the traceback in the page
+        err_pre = py_repl.locator("div.py-repl-output > pre.py-error")
+        tb_lines = err_pre.inner_text().splitlines()
+        assert tb_lines[0] == "Traceback (most recent call last):"
+        assert tb_lines[-1] == "Exception: this is an error"
+
+    def test_python_exception_after_previous_output(self):
+        self.pyscript_run(
+            """
+            <py-repl>
+                display('hello world')
+            </py-repl>
+            """
+        )
+        py_repl = self.page.locator("py-repl")
+        out_div = py_repl.locator("div.py-repl-output")
         self.page.keyboard.press("Shift+Enter")
-        expect(self.page.locator(".py-error")).to_be_visible()
-        self.page.keyboard.press("Backspace")
-        self.page.locator("py-repl").type("display('ok')")
+        assert out_div.inner_text() == "hello world"
+        #
+        # clear the editor, write new code, execute
+        self._replace(py_repl, "0/0")
         self.page.keyboard.press("Shift+Enter")
-        repl_result = self.page.wait_for_selector("#my-repl-2", state="attached")
-        assert repl_result.inner_text() == "ok"
+        assert "hello world" not in out_div.inner_text()
+        assert "ZeroDivisionError" in out_div.inner_text()
+
+    def test_hide_previous_error_after_successful_run(self):
+        """
+        this tests the fact that a new error div should be created once there's an
+        error but also that it should disappear automatically once the error
+        is fixed
+        """
+        self.pyscript_run(
+            """
+            <py-repl>
+                raise Exception('this is an error')
+            </py-repl>
+            """
+        )
+        py_repl = self.page.locator("py-repl")
+        out_div = py_repl.locator("div.py-repl-output")
+        self.page.keyboard.press("Shift+Enter")
+        assert "this is an error" in out_div.inner_text()
+        #
+        self._replace(py_repl, "display('hello')")
+        self.page.keyboard.press("Shift+Enter")
+        assert out_div.inner_text() == "hello"
+
+    def test_output_attribute(self):
+        self.pyscript_run(
+            """
+            <py-repl output="mydiv">
+                display('hello world')
+            </py-repl>
+            <hr>
+            <div id="mydiv"></div>
+            """
+        )
+        py_repl = self.page.locator("py-repl")
+        py_repl.locator("button").click()
+        #
+        # check that we did NOT write to py-repl-output
+        out_div = py_repl.locator("div.py-repl-output")
+        assert out_div.inner_text() == ""
+        # check that we are using mydiv instead
+        mydiv = self.page.locator("#mydiv")
+        assert mydiv.inner_text() == "hello world"
+
+    def test_output_attribute_does_not_exist(self):
+        """
+        If we try to use an attribute which doesn't exist, we display an error
+        instead
+        """
+        self.pyscript_run(
+            """
+            <py-repl output="I-dont-exist">
+                print('I will not be executed')
+            </py-repl>
+            """
+        )
+        py_repl = self.page.locator("py-repl")
+        py_repl.locator("button").click()
+        #
+        out_div = py_repl.locator("div.py-repl-output")
+        msg = "py-repl ERROR: cannot find the output element #I-dont-exist in the DOM"
+        assert out_div.inner_text() == msg
+        assert "I will not be executed" not in self.console.log.text

commit ddfcac56b9197e72472369f18208db185884a15f

What is not tested is broken. Test&fix <py-repl auto-generate=true> which was broken by PR #884

pyscriptjs/src/components/pyrepl.ts
@@ -173,6 +173,8 @@ export function make_PyRepl(runtime: Runtime) {
             if (pyResult !== undefined) {
                 pyDisplay(runtime, pyResult, { target: outEl.id });
             }
+
+            this.autogenerateMaybe();
         }
 
         getPySrc(): string {

pyscriptjs/tests/integration/support.py
@@ -275,6 +275,15 @@ class PyScriptTest:
         if wait_for_pyscript:
             self.wait_for_pyscript()
 
+    def iter_locator(self, loc):
+        """
+        Helper method to iterate over all the elements which are matched by a
+        locator, siny playwright does not seem to support it natively.
+        """
+        n = loc.count()
+        elems = [loc.nth(i) for i in range(n)]
+        return iter(elems)
+
 
 # ============== Helpers and utility functions ==============
 

pyscriptjs/tests/integration/test_00_support.py
@@ -338,3 +338,20 @@ class TestSupport(PyScriptTest):
         self.wait_for_console("Page loaded!", timeout=200, check_js_errors=False)
         # clear the errors, else the test fails at teardown
         self.clear_js_errors()
+
+    def test_iter_locator(self):
+        doc = """
+        <html>
+          <body>
+              <div>foo</div>
+              <div>bar</div>
+              <div>baz</div>
+          </body>
+        </html>
+        """
+        self.writefile("mytest.html", doc)
+        self.goto("mytest.html")
+        divs = self.page.locator("div")
+        assert divs.count() == 3
+        texts = [el.inner_text() for el in self.iter_locator(divs)]
+        assert texts == ["foo", "bar", "baz"]

pyscriptjs/tests/integration/test_py_repl.py
@@ -229,3 +229,30 @@ class TestPyRepl(PyScriptTest):
         msg = "py-repl ERROR: cannot find the output element #I-dont-exist in the DOM"
         assert out_div.inner_text() == msg
         assert "I will not be executed" not in self.console.log.text
+
+    def test_auto_generate(self):
+        self.pyscript_run(
+            """
+            <py-repl auto-generate="true">
+            </py-repl>
+            """
+        )
+        py_repls = self.page.locator("py-repl")
+        outputs = py_repls.locator("div.py-repl-output")
+        assert py_repls.count() == 1
+        assert outputs.count() == 1
+        #
+        # evaluate the py-repl and generate another one
+        self.page.keyboard.type("'hello'")
+        self.page.keyboard.press("Shift+Enter")
+        assert py_repls.count() == 2
+        assert outputs.count() == 2
+        # now we type something else: the new py-repl should have the focus
+        self.page.keyboard.type("'world'")
+        self.page.keyboard.press("Shift+Enter")
+        assert py_repls.count() == 3
+        assert outputs.count() == 3
+        #
+        # check that the code and the outputs are in order
+        out_texts = [el.inner_text() for el in self.iter_locator(outputs)]
+        assert out_texts == ["hello", "world", ""]