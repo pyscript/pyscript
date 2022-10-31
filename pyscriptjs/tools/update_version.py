1ca636964412bc866997a3762e9bd804d42c7564
Jeff Glass <glass.jeffrey@gmail.com>
2022-10-31 09:55:05 -0500

    add update_vesion.py

---
+++ pyscriptjs/tools/update_version.py
@@ -0,0 +1,96 @@
+import argparse
+import re
+import json
+import os
+from pathlib import Path
+from typing import Callable
+
+version_pattern = '(export const version:JSON = <JSON><unknown>)({.+})'
+
+def get_runtimets_path() -> Path:
+    """
+    Get the absolute path of 'runtime.ts'
+
+    Since we don't always know what the current working directory
+    will be (in tests/builds/workflows etc), search the file path
+    for "pyscript/pyscriptjs" and work up from there.
+    """
+
+    currentPath = Path(os.path.abspath(__file__)).resolve()
+    pycsriptjs_path = [p for p in currentPath.parents if p.stem == "pyscriptjs"][0]
+    runtime_path = pycsriptjs_path / 'src' / 'runtime.ts'
+    return runtime_path
+
+def get_current_version(runtime_path) -> dict:
+    """
+    Get the 
+    """
+    with open(runtime_path, 'r') as fp:
+        return json.loads(re.search(version_pattern, fp.read()).group(2))
+        
+def set_version(**kwargs) -> None:
+    print(f"Setting version with {kwargs}")
+    runtime_path = get_runtimets_path()
+
+    version_info = get_current_version(runtime_path)
+
+    for key, value in kwargs.items():
+        if key in version_info:
+            version_info[key] = value
+        else:
+            raise KeyError(f"set_version() only accepts the following keys: {*version_info.keys(),}")
+
+    with open(runtime_path, 'r') as fp:
+        updated_runtime = re.sub(version_pattern, lambda m: m.group(1) + str(version_info).replace("'", '"') , fp.read())
+
+    with open(runtime_path, 'w') as fp:
+        fp.write(updated_runtime)
+
+def int_with_length(length: int) -> Callable[[str], int]:
+    def validator(arg: str) -> int:
+        if len(arg) != length:
+            raise argparse.ArgumentError(f"Argument must be of length {length}")
+        return int(arg)
+    return validator
+
+
+def init_argparse() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description = "Set or update the PyScript version"
+    )
+
+    parser.add_argument(
+        "-y", "--year",
+        type=int_with_length(4),
+        help="The year field of the verson [YYYY].[MM].[(patch)].releaselevel"
+    )
+
+    parser.add_argument(
+        "-m", "--month",
+        type=int,
+        help="The month field of the verson [YYYY].[MM].[(patch)].releaselevel"
+    )
+
+    parser.add_argument(
+        "-p", "--patch",
+        type=int,
+        help="The year field of the verson [YYYY].[MM].[(patch)].releaselevel"
+    )
+
+    parser.add_argument(
+        "--releaselevel",
+        type=str,
+        help="The year field of the verson [YYYY].[MM].[(patch)].releaselevel"
+    )
+    return parser
+
+
+if __name__ == "__main__":
+    parser = init_argparse()
+    args = vars(parser.parse_args())
+
+    usable_args = {a:args[a] for a in args if (args[a] is not None)}
+    print(usable_args)
+    
+    if usable_args: set_version(**usable_args)
+    else: print("No arguments were passed to update_version.py")
