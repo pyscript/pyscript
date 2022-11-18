01bd0947a79cacf2fefae4542091a2070cdda0b3
Fabio Pliger <fabio.pliger@gmail.com>
2022-11-18 12:47:29 -0600

    remove unused PyPlugin

--- pyscriptjs/src/plugin.ts
+++ pyscriptjs/src/plugin.ts
@@ -58,47 +58,10 @@ export class Plugin {
     }
 }
 
-export class PyPlugin extends HTMLElement {
-
-    /** Validate the configuration of the plugin and handle default values.
-     *
-     * Individual plugins are expected to check that the config keys/sections
-     * which are relevant to them contains valid values, and to raise an error
-     * if they contains unknown keys.
-     *
-     * This is also a good place where set default values for those keys which
-     * are not specified by the user.
-     *
-     * This hook should **NOT** contain expensive operations, else it delays
-     * the download of the python interpreter which is initiated later.
-     */
-    configure(config: AppConfig) {
-    }
-
-    /** The Python interpreter has been launched, the virtualenv has been
-      * installed and we are ready to execute user code.
-      *
-      * The <py-script> tags will be executed after this hook.
-      */
-    afterSetup(runtime: Runtime) {
-    }
-
-    /** Startup complete. The interpreter is initialized and ready, user
-     * scripts have been executed: the main initialization logic ends here and
-     * the page is ready to accept user interactions.
-     */
-    afterStartup(runtime: Runtime) {
-    }
-
-    /** Called when an UserError is raised
-     */
-    onUserError(error: UserError) {
-    }
-}
 
 export class PluginManager {
     _plugins: Plugin[];
-    _pythonPlugins: PyPlugin[];
+    _pythonPlugins: any[];
 
     constructor() {
         this._plugins = [];
@@ -110,7 +73,7 @@ export class PluginManager {
             this._plugins.push(p);
     }
 
-    _addPythonPlugin(plugin: PyPlugin){
+    _addPythonPlugin(plugin: any){
         this._pythonPlugins.push(plugin);
     }
 
