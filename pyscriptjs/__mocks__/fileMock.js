af24043d18722eee0d846e72766d14b66a4b60eb
Jeff Glass <glass.jeffrey@gmail.com>
2022-11-18 09:10:07 -0600

    Mock pyscript.py with __mocks__/pyscript.js

--- pyscriptjs/__mocks__/fileMock.js
+++ pyscriptjs/__mocks__/fileMock.js
@@ -1,12 +1,16 @@
 /**
- * this file mocks the `src/python/pyscript.py` file
- * since importing of `.py` files isn't usually supported
- * inside JS/TS files.
+ * this file mocks python files that are not explicitly
+ * match by a regex in jest.config.js importing of
+ * `.py` files isn't usually supported inside JS/TS files.
  *
- * It sets the value of whatever is imported from
- * `src/python/pyscript.py` to be an empty string i.e. ""
+ * It sets the value of whatever is import from that
+ * python file to ""
  *
  * This is needed since the imported object is further
  * passed to a function which only accepts a string.
  */
-module.exports = "";
+
+ console.warn(`.py files that are not explicitly mocked in \
+ jest.config.js and /__mocks__/ are mocked as empty strings`)
+
+ module.exports = ""
