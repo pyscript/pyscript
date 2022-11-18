0de8191cc541a90f182ae2d6f7b06d1f1bf96f34
Fabio Pliger <fabio.pliger@gmail.com>
2022-11-17 19:42:04 -0600

    add example hook onto markdown plugin

--- pyscriptjs/src/plugins/python/py_markdown.py
+++ pyscriptjs/src/plugins/python/py_markdown.py
@@ -2,10 +2,18 @@ from textwrap import dedent
 
 from markdown import markdown
 
-from pyscript import Plugin
+from pyscript import Plugin, console
 
-plugin = Plugin("py-markdown")  # priority is optional, of course
+# plugin = Plugin("py-markdown")  # priority is optional, of course
 
+class MyPlugin(Plugin):
+    def configure(self, config):
+        console.log(f"GOT CONFIG!! {config}")
+
+    def afterStartup(self, runtime):
+        console.log(f"GOT RUNTIME!! {runtime}")
+
+plugin = MyPlugin("py-markdown")
 
 @plugin.register_custom_element("py-md")
 class PyMarkdown:
