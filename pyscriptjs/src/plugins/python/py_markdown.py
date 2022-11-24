95d095b8b66387bd09629c81f56b78bcc504919c
Fabio Pliger <fabio.pliger@gmail.com>
2022-11-24 11:40:37 -0600

    add warning to py_markdown plugin

--- pyscriptjs/src/plugins/python/py_markdown.py
+++ pyscriptjs/src/plugins/python/py_markdown.py
@@ -3,6 +3,10 @@ from textwrap import dedent
 from markdown import markdown
 from pyscript import Plugin, console
 
+console.warning(
+    'WARNING: This plugin is still in a very experimental phase and will likely change'\
+    ' and potentially break in the future releases. Use it with caution.'
+    )
 
 class MyPlugin(Plugin):
     def configure(self, config):
