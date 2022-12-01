13127fd690e40a78a65db2c620f3cca16207997f
mariana <marianameireles@protonmail.com>
2022-12-02 00:07:06 +0100

    test 864now works consistently

--- pyscriptjs/tests/integration/test_zz_examples.py
+++ pyscriptjs/tests/integration/test_zz_examples.py
@@ -256,7 +256,6 @@ class TestExamples(PyScriptTest):
         assert self.page.title() == "PyScript/Panel Streaming Demo"
         wait_for_render(self.page, "*", "<div.*?class=['\"]bk-root['\"].*?>")
 
-    # @pytest.mark.skip("flaky test, see issue #864")
     def test_repl(self):
         self.goto("examples/repl.html")
         self.wait_for_pyscript()
