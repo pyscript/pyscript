508b4e7e1a6a663a8611cf5e792d779c008595ec
Fabio Pliger <fpliger@anaconda.com>
2022-12-08 10:40:40 -0600

    remove commented code

--- pyscriptjs/tests/py-unit/test_python_plugins.py
+++ pyscriptjs/tests/py-unit/test_python_plugins.py
@@ -17,16 +17,3 @@ class TestPyMarkdown:
         
         py_markdown.plugin.afterStartup(runtime)
         console_mock.log.assert_called_with("runtime received: just a runtime")
-
-    # def test_connected(self, monkeypatch):
-    #     el = pyscript.Element("something")
-    #     document_mock = Mock()
-    #     call_result = "some_result"
-    #     document_mock.querySelector = Mock(return_value=call_result)
-    #     monkeypatch.setattr(pyscript, "document", document_mock)
-    #     assert not el._element
-    #     real_element = el.element
-    #     assert real_element
-    #     assert pyscript.document.querySelector.call_count == 1
-    #     pyscript.document.querySelector.assert_called_with("#something")
-    #     assert real_element == call_result
