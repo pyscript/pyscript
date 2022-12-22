c863e18bd53c35ef9203e7d6a0cb58b4ff461e79
Jeff Glass <glass.jeffrey@gmail.com>
2022-12-22 11:19:39 -0600

    Add TS tests

--- pyscriptjs/tests/unit/stdio.test.ts
+++ pyscriptjs/tests/unit/stdio.test.ts
@@ -1,4 +1,5 @@
-import { type Stdio, CaptureStdio, StdioMultiplexer } from '../../src/stdio';
+import { expect } from '@jest/globals';
+import { type Stdio, CaptureStdio, StdioMultiplexer, TargetedStdio } from '../../src/stdio';
 
 describe('CaptureStdio', () => {
     it('captured streams are initialized to empty string', () => {
@@ -63,5 +64,53 @@ describe('StdioMultiplexer', () => {
 
         expect(b.captured_stdout).toBe("out 2\n");
         expect(b.captured_stderr).toBe("err 2\n");
+    });
+});
+
+describe('TargetedStdio', () => {
+    let capture: CaptureStdio;
+    let targeted: TargetedStdio;
+    let multi: StdioMultiplexer;
+
+    beforeEach(() => {
+        capture = new CaptureStdio();
+        targeted = new TargetedStdio('target-id');
+
+        multi = new StdioMultiplexer();
+        multi.addListener(capture)
+        multi.addListener(targeted)
+
+        let div = document.getElementById("target-id")
+        if ( div=== null){
+            div = document.createElement('div')
+            div.id = "target-id"
+            document.body.appendChild(div)
+        }
+        else {
+            div.innerHTML = ""
+        }
+    });
+
+    it('targeted id is set by constructor', () =>{
+        expect(targeted.target_id).toBe("target-id");
+    });
+
+    it('targeted stdio/stderr also goes to multiplexer', () =>{
+        multi.stdout_writeline("out 1");
+        multi.stderr_writeline("out 2");
+        expect(capture.captured_stdout).toBe("out 1\n");
+        expect(capture.captured_stderr).toBe("out 2\n");
+        expect(document.getElementById("target-id")?.innerHTML).toBe("out 1<br>out 2<br>");
+    });
+
+    it('Add and remove targeted listener', () => {
+        multi.stdout_writeline("out 1");
+        multi.removeListener(targeted);
+        multi.stdout_writeline("out 2");
+        multi.addListener(targeted);
+        multi.stdout_writeline("out 3");
+
+        expect(capture.captured_stdout).toBe("out 1\nout 2\nout 3\n");
+        expect(document.getElementById("target-id")?.innerHTML).toBe("out 1<br>out 3<br>");
     });
 });
