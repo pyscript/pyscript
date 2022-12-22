6c0f0b0c7c54c155044e98dd1a95dd96f248f7f5
Jeff Glass <glass.jeffrey@gmail.com>
2022-12-22 09:25:46 -0600

    Formatting, assert_no_banners()

--- pyscriptjs/tests/integration/test_stdio_handling.py
+++ pyscriptjs/tests/integration/test_stdio_handling.py
@@ -7,21 +7,21 @@ class TestOutputHandling(PyScriptTest):
     def test_targeted_stdio(self):
         self.pyscript_run(
             """
-        <py-config>
-            terminal = true
-        </py-config>
-        <py-terminal></py-terminal>
-        <div id="container">
-            <div id="first"></div>
-            <div id="second"></div>
-            <div id="third"></div>
-        </div>
-        <py-script output="first">print("first 1.")</py-script>
-        <py-script output="second">print("second.")</py-script>
-        <py-script output="third">print("third.")</py-script>
-        <py-script output="first">print("first 2.")</py-script>
-        <py-script>print("no output.")</py-script>
-        """
+            <py-config>
+                terminal = true
+            </py-config>
+            <py-terminal></py-terminal>
+            <div id="container">
+                <div id="first"></div>
+                <div id="second"></div>
+                <div id="third"></div>
+            </div>
+            <py-script output="first">print("first 1.")</py-script>
+            <py-script output="second">print("second.")</py-script>
+            <py-script output="third">print("third.")</py-script>
+            <py-script output="first">print("first 2.")</py-script>
+            <py-script>print("no output.")</py-script>
+            """
         )
 
         # Check that page has desired parent/child structure, and that
@@ -52,16 +52,18 @@ class TestOutputHandling(PyScriptTest):
             assert line_index > last_index
             last_index = line_index
 
+        self.assert_no_banners()
+
     def test_stdio_escape(self):
         # Test that text that looks like HTML tags is properly escaped in stdio
         self.pyscript_run(
             """
-        <div id="first"></div>
-        <py-script output="first">
-            print("<p>Hello</p>")
-            print('<img src="https://example.net">')
-        </py-script>
-        """
+            <div id="first"></div>
+            <py-script output="first">
+                print("<p>Hello</p>")
+                print('<img src="https://example.net">')
+            </py-script>
+            """
         )
 
         text = self.page.locator("#first").text_content()
@@ -69,21 +71,23 @@ class TestOutputHandling(PyScriptTest):
         assert "<p>Hello</p>" in text
         assert '<img src="https://example.net">' in text
 
+        self.assert_no_banners()
+
     def test_targeted_stdio_linebreaks(self):
         self.pyscript_run(
             """
-        <div id="first"></div>
-        <py-script output="first">
-            print("one.")
-            print("two.")
-            print("three.")
-        </py-script>
+            <div id="first"></div>
+            <py-script output="first">
+                print("one.")
+                print("two.")
+                print("three.")
+            </py-script>
 
-        <div id="second"></div>
-        <py-script output="second">
-            print("one.\\ntwo.\\nthree.")
-        </py-script>
-        """
+            <div id="second"></div>
+            <py-script output="second">
+                print("one.\\ntwo.\\nthree.")
+            </py-script>
+            """
         )
 
         # check line breaks at end of each input
@@ -92,39 +96,41 @@ class TestOutputHandling(PyScriptTest):
         # new lines are converted to line breaks
         assert self.page.locator("#second").inner_html() == "one.<br>two.<br>three.<br>"
 
+        self.assert_no_banners()
+
     def test_targeted_stdio_async(self):
         # Test the behavior of stdio capture in async contexts
         self.pyscript_run(
             """
-        <py-script>
-            import asyncio
-            import js
+            <py-script>
+                import asyncio
+                import js
 
-            async def coro(value, delay):
-                print(value)
-                await asyncio.sleep(delay)
-                js.console.log(f"DONE {value}")
-        </py-script>
+                async def coro(value, delay):
+                    print(value)
+                    await asyncio.sleep(delay)
+                    js.console.log(f"DONE {value}")
+            </py-script>
 
-        <div id="first"></div>
-        <py-script>
-            asyncio.ensure_future(coro("first", 1))
-        </py-script>
+            <div id="first"></div>
+            <py-script>
+                asyncio.ensure_future(coro("first", 1))
+            </py-script>
 
-        <div id="second"></div>
-        <py-script output="second">
-            asyncio.ensure_future(coro("second", 1))
-        </py-script>
+            <div id="second"></div>
+            <py-script output="second">
+                asyncio.ensure_future(coro("second", 1))
+            </py-script>
 
-        <div id="third"></div>
-        <py-script output="third">
-            asyncio.ensure_future(coro("third", 0))
-        </py-script>
+            <div id="third"></div>
+            <py-script output="third">
+                asyncio.ensure_future(coro("third", 0))
+            </py-script>
 
-        <py-script output="third">
-            asyncio.ensure_future(coro("DONE", 3))
-        </py-script>
-        """
+            <py-script output="third">
+                asyncio.ensure_future(coro("DONE", 3))
+            </py-script>
+            """
         )
 
         self.wait_for_console("DONE DONE")
@@ -138,30 +144,32 @@ class TestOutputHandling(PyScriptTest):
         assert self.page.locator("#second").text_content() == ""
         assert self.page.locator("#third").text_content() == ""
 
+        self.assert_no_banners()
+
     def test_targeted_stdio_interleaved(self):
         # Test that synchronous writes to stdout are placed correctly, even
         # While interleaved with scheduling coroutines in the same tag
         self.pyscript_run(
             """
-        <div id="good"></div>
-        <div id="bad"></div>
-        <py-script output="good">
-            import asyncio
-            import js
+            <div id="good"></div>
+            <div id="bad"></div>
+            <py-script output="good">
+                import asyncio
+                import js
 
-            async def coro_bad(value, delay):
-                print(value)
-                await asyncio.sleep(delay)
+                async def coro_bad(value, delay):
+                    print(value)
+                    await asyncio.sleep(delay)
 
-            print("one.")
-            asyncio.ensure_future(coro_bad("badone.", 0.1))
-            print("two.")
-            asyncio.ensure_future(coro_bad("badtwo.", 0.2))
-            print("three.")
-            asyncio.ensure_future(coro_bad("badthree.", 0))
-            asyncio.ensure_future(coro_bad("DONE", 1))
-        </py-script>
-        """
+                print("one.")
+                asyncio.ensure_future(coro_bad("badone.", 0.1))
+                print("two.")
+                asyncio.ensure_future(coro_bad("badtwo.", 0.2))
+                print("three.")
+                asyncio.ensure_future(coro_bad("badthree.", 0))
+                asyncio.ensure_future(coro_bad("DONE", 1))
+            </py-script>
+            """
         )
 
         # No text should appear from coroutines
@@ -176,26 +184,28 @@ class TestOutputHandling(PyScriptTest):
             assert (line_index := self.console.log.lines.index(line)) > -1
             assert line_index > last_index
 
+        self.assert_no_banners()
+
     def test_targeted_stdio_dynamic_tags(self):
         # Test that creating py-script tags via Python still leaves
         # stdio targets working
 
         self.pyscript_run(
             """
-        <div id="first"></div>
-        <div id="second"></div>
-        <py-script output="first">
-            print("first.")
+            <div id="first"></div>
+            <div id="second"></div>
+            <py-script output="first">
+                print("first.")
 
-            import js
-            tag = js.document.createElement("py-script")
-            tag.innerText = "print('second.')"
-            tag.setAttribute("output", "second")
-            js.document.body.appendChild(tag)
+                import js
+                tag = js.document.createElement("py-script")
+                tag.innerText = "print('second.')"
+                tag.setAttribute("output", "second")
+                js.document.body.appendChild(tag)
 
-            print("first.")
-        </py-script>
-        """
+                print("first.")
+            </py-script>
+            """
         )
 
         # Ensure second tag was added to page
@@ -205,24 +215,26 @@ class TestOutputHandling(PyScriptTest):
         assert self.page.locator("#first").text_content() == "first.first."
         assert second_div.text_content() == "second."
 
+        self.assert_no_banners()
+
     def test_stdio_id_errors(self):
         # Test that using an ID not present on the page as the Output
         # Attribute creates exactly 1 warning banner
         self.pyscript_run(
             """
-        <py-script output="not-on-page">
-            print("bad.")
-        </py-script>
+            <py-script output="not-on-page">
+                print("bad.")
+            </py-script>
 
-        <div id="on-page"></div>
-        <py-script>
-            print("good.")
-        </py-script>
+            <div id="on-page"></div>
+            <py-script>
+                print("good.")
+            </py-script>
 
-        <py-script output="not-on-page">
-            print("bad.")
-        </py-script>
-        """
+            <py-script output="not-on-page">
+                print("bad.")
+            </py-script>
+            """
         )
 
         banner = self.page.query_selector_all(".py-warning")
@@ -238,13 +250,14 @@ class TestOutputHandling(PyScriptTest):
         # Test that stderr works, and routes to the same location as stdout
         self.pyscript_run(
             """
-        <div id="first"></div>
-        <py-script output="first">
-            import sys
-            print("one.", file=sys.stderr)
-            print("two.")
-        </py-script>
-        """
+            <div id="first"></div>
+            <py-script output="first">
+                import sys
+                print("one.", file=sys.stderr)
+                print("two.")
+            </py-script>
+            """
         )
 
         assert self.page.locator("#first").text_content() == "one.two."
+        self.assert_no_banners()
