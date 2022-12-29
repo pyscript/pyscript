ce029ef53c53481f577ea86cf6cd04eec7b869c0
FabioRosado <fabiorosado@outlook.com>
2022-12-29 16:11:19 +0000

    Rename runtime tests

--- pyscriptjs/tests/integration/test_interpreter.py
+++ pyscriptjs/tests/integration/test_interpreter.py
@@ -1,10 +1,10 @@
 from .support import PyScriptTest
 
 
-class TestRuntimeAccess(PyScriptTest):
-    """Test accessing Python objects from JS via pyscript.runtime"""
+class TestInterpreterAccess(PyScriptTest):
+    """Test accessing Python objects from JS via pyscript.interpreter"""
 
-    def test_runtime_python_access(self):
+    def test_interpreter_python_access(self):
         self.pyscript_run(
             """
             <py-script>
@@ -17,8 +17,8 @@ class TestRuntimeAccess(PyScriptTest):
 
         self.page.add_script_tag(
             content="""
-        console.log(`x is ${pyscript.runtime.globals.get('x')}`);
-        console.log(`py_func() returns ${pyscript.runtime.globals.get('py_func')()}`);
+        console.log(`x is ${pyscript.interpreter.globals.get('x')}`);
+        console.log(`py_func() returns ${pyscript.interpreter.globals.get('py_func')()}`);
         """
         )
 
@@ -28,14 +28,14 @@ class TestRuntimeAccess(PyScriptTest):
             "py_func() returns 2",
         ]
 
-    def test_runtime_script_execution(self):
-        """Test running Python code from js via pyscript.runtime"""
+    def test_interpreter_script_execution(self):
+        """Test running Python code from js via pyscript.interpreter"""
         self.pyscript_run("")
 
         self.page.add_script_tag(
             content="""
-        const interpreter = pyscript.runtime.interpreter;
-        interpreter.runPython('console.log("Interpreter Ran This")');
+        const interface = pyscript.interpreter.interface;
+        interface.runPython('console.log("Interpreter Ran This")');
         """
         )
         assert self.console.log.lines[0] == self.PY_COMPLETE
