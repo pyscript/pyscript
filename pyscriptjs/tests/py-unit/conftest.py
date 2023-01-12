cf634582f116e5faf971968fbed410cf59bbf159
Fabio Pliger <fpliger@anaconda.com>
2023-01-11 20:18:51 -0600

    add plugins manager fixture and init plugins tests helper in conftest

--- pyscriptjs/tests/py-unit/conftest.py
+++ pyscriptjs/tests/py-unit/conftest.py
@@ -2,6 +2,8 @@
 import pathlib
 import sys
 
+import pytest
+
 # current working directory
 base_path = pathlib.Path().absolute()
 # add pyscript folder to path
@@ -11,3 +13,16 @@ sys.path.append(str(python_source))
 # add Python plugins folder to path
 python_plugins_source = base_path / "src" / "plugins" / "python"
 sys.path.append(str(python_plugins_source))
+
+import pyscript_plugins_tester as ppt  # noqa: E402
+
+# patch pyscript module where needed
+import pyscript  # noqa: E402
+
+pyscript.define_custom_element = ppt.define_custom_element
+
+
+@pytest.fixture()
+def plugins_manager():
+    """return a new instance of a Test version the PyScript application plugins manager"""
+    return ppt.plugins_manager  # PluginsManager()
