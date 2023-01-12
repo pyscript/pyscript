4a716b12cd59c8945b91384060df98b32daae32d
Fabio Pliger <fpliger@anaconda.com>
2023-01-11 21:07:36 -0600

    add test for py_tutor

--- pyscriptjs/tests/py-unit/pyodide/pyodide.py
+++ pyscriptjs/tests/py-unit/pyodide/pyodide.py
@@ -1,4 +1,4 @@
 """Mock module that emulates some of the pyodide js module features for the sake of tests"""
 from unittest.mock import Mock
 
-create_proxy = Mock()
+create_proxy = Mock(side_effect=lambda x: x)
--- pyscriptjs/tests/py-unit/test_python_plugins.py
+++ pyscriptjs/tests/py-unit/test_python_plugins.py
@@ -1,6 +1,11 @@
+from textwrap import dedent
 from unittest.mock import Mock
 
 import py_markdown
+import py_tutor
+import pyscript_plugins_tester as ppt
+
+import pyscript
 
 
 class TestPyMarkdown:
@@ -15,3 +20,88 @@ class TestPyMarkdown:
 
         py_markdown.plugin.afterStartup(runtime)
         console_mock.log.assert_called_with("runtime received: just a runtime")
+
+
+class TestPyTutor:
+    def check_prism_added(self):
+        head = pyscript.js.document.head
+
+        # EXPECT the head elements to be present and correctly configured
+        links = head.getElementsByTagName("link")
+        assert len(links) == 1
+        link = links[0]
+        assert link.type == "text/css"
+        assert link.rel == "stylesheet"
+        assert link.href == "./assets/prism/prism.css"
+
+        scripts = head.getElementsByTagName("script")
+        assert len(scripts) == 1
+        script = scripts[0]
+        assert script.type == "text/javascript"
+        assert script.src == "./assets/prism/prism.js"
+
+        # TODO: Check this because seems wrong. It should have src and not really PAGE_SCRIPT?
+
+        # Check the actual JS script code
+        # To do so we have 2 methods (it depends on browser support so we check either...)
+        if script.childNodes:
+            # in this case it means the content has been added as a child element
+            node = script.childNodes[0]
+            assert node.data == py_tutor.PAGE_SCRIPT
+
+    def check_append_script_to_page(self):
+        body = pyscript.js.document.body
+        scripts = body.getElementsByTagName("script")
+        assert len(scripts) == 1
+        script = scripts[0]
+        assert script.type == "text/javascript"
+
+        # Check the actual JS script code
+        # To do so we have 2 methods (it depends on browser support so we check either...)
+        if script.childNodes:
+            # in this case it means the content has been added as a child element
+            node = script.childNodes[0]
+            assert node.data == py_tutor.PAGE_SCRIPT
+
+    def test_connected_calls(self, plugins_manager: ppt.PluginsManager):
+        # add the plugin to the app plugin manager
+        # this will:
+        # create
+        plugins_manager.addPythonPlugin(py_tutor.plugin)
+        tutor_ce = plugins_manager._custom_elements["py-tutor"]
+        # tutor_ce_python_instance = tutor_ce.pyPluginInstance
+        # GIVEN: The following innerHTML on the ce elements
+        tutor_ce.innerHTML = dedent(
+            """
+        <py-config>
+          packages = [
+            "folium",
+            "pandas"
+          ]
+          plugins = [
+            "../build/plugins/python/py_tutor.py"
+          ]
+        </py-config>
+
+        <py-script>
+        import folium
+        import json
+        import pandas as pd
+
+        from pyodide.http import open_url
+
+        # the rest of the code goes one
+        </py-script>
+        """
+        )
+
+        # GIVEN: the CustomElement connectedCallback gets called
+        tutor_ce.connectedCallback()
+
+        # EXPECT: the
+        self.check_prism_added()
+
+        self.check_append_script_to_page()
+
+    def test_plugin_registered(self):
+        pass
