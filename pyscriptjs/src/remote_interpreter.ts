2c60d734e4bdca0e126df3d6dfaddbb077f15c29
Madhur Tandon <madhurtandon23@gmail.com>
2023-03-03 02:42:27 +0530

    try mkdirTree

--- pyscriptjs/src/remote_interpreter.ts
+++ pyscriptjs/src/remote_interpreter.ts
@@ -200,54 +200,26 @@ export class RemoteInterpreter extends Object {
      * Example usage:
      * await loadFromFile(`a/b/c/foo.py`, `http://dummy.com/hi.py`)
      *
-     * Nested paths are iteratively analysed and each part is created
-     * if it doesn't exist.
-     *
-     * The analysis returns if the part exists and if it's parent directory exists
-     * Due to the manner in which we proceed, the parent will ALWAYS exist.
-     *
-     * The iteration proceeds in the following manner for `a/b/c/foo.py`:
-     *
-     * - `a` doesn't exist but it's parent i.e. `root` exists --> create `a`
-     * - `a/b` doesn't exist but it's parent i.e. `a` exists --> create `a/b`
-     * - `a/b/c` doesn't exist but it's parent i.e. `a/b` exists --> create `a/b/c`
-     *
-     * Finally, write content of `http://dummy.com/hi.py` to `a/b/c/foo.py`
+     * Write content of `http://dummy.com/hi.py` to `a/b/c/foo.py`
      *
      * NOTE: The `path` parameter expects to have the `filename` in it i.e.
      * `a/b/c/foo.py` is valid while `a/b/c` (i.e. only the folders) are incorrect.
+     *
+     * Also, the path shouldn't be relative i.e. cannot be like './a/b/c'
      */
     async loadFromFile(path: string, fetch_path: string): Promise<void> {
         const pathArr = path.split('/');
-        const filename = pathArr.pop();
-        for (let i = 0; i < pathArr.length; i++) {
-            // iteratively calculates parts of the path i.e. `a`, `a/b`, `a/b/c` for `a/b/c/foo.py`
-            const eachPath = pathArr.slice(0, i + 1).join('/');
-
-            // analyses `eachPath` and returns if it exists along with if its parent directory exists or not
-            const { exists, parentExists } = this.interface.FS.analyzePath(eachPath);
-
-            // due to the iterative manner in which we proceed, the parent directory should ALWAYS exist
-            if (!parentExists) {
-                throw new Error(`'INTERNAL ERROR! cannot create ${path}, this should never happen'`);
-            }
-
-            // creates `eachPath` if it doesn't exist
-            if (!exists) {
-                this.interface.FS.mkdir(eachPath);
-            }
-        }
+        // pop out the filename
+        pathArr.pop();
+        const dirTree = pathArr.join('/');
+        this.interface.FS.mkdirTree(dirTree);
 
         // `robustFetch` checks for failures in getting a response
         const response = await robustFetch(fetch_path);
         const buffer = await response.arrayBuffer();
         const data = new Uint8Array(buffer);
 
-        pathArr.push(filename);
-        // opens a file descriptor for the file at `path`
-        const stream = this.interface.FS.open(pathArr.join('/'), 'w');
-        this.interface.FS.write(stream, data, 0, data.length, 0);
-        this.interface.FS.close(stream);
+        this.interface.FS.writeFile(path, data, {canOwn: true});
     }
 
     /**
