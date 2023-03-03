4483f0db0f69d3930c3cefa03f6e2ce973910149
Hood Chatham <roberthoodchatham@gmail.com>
2023-03-03 18:32:06 +0100

    Move prism.js and prism.css to prism.min.* (#1248)

--- pyscriptjs/src/plugins/python/py_tutor.py
+++ pyscriptjs/src/plugins/python/py_tutor.py
@@ -85,12 +85,12 @@ class PyTutor:
         link.type = "text/css"
         link.rel = "stylesheet"
         js.document.head.appendChild(link)
-        link.href = "./assets/prism/prism.css"
+        link.href = "./assets/prism/prism.min.css"
 
         # Add the JS file
         script = js.document.createElement("script")
         script.type = "text/javascript"
-        script.src = "./assets/prism/prism.js"
+        script.src = "./assets/prism/prism.min.js"
         js.document.head.appendChild(script)
 
     def _create_code_section(self, source, module_paths=None, parent=None):
