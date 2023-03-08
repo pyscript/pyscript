2e158d485a59ea89dffca32fedc3346870f94543
Robert Hood Chatham <roberthoodchatham@gmail.com>
2023-03-08 21:34:02 +0100

    Try @iarna/toml instead

--- pyscriptjs/src/pyconfig.ts
+++ pyscriptjs/src/pyconfig.ts
@@ -145,17 +145,19 @@ function mergeConfig(inlineConfig: AppConfig, externalConfig: AppConfig): AppCon
 
 function parseConfig(configText: string, configType = 'toml') {
     if (configType === 'toml') {
+        // TOML parser is soft and can parse even JSON strings, this additional check prevents it.
+        if (configText.trim()[0] === '{') {
+            throw new UserError(
+                ErrorCode.BAD_CONFIG,
+                `The config supplied: ${configText} is an invalid TOML and cannot be parsed`,
+            );
+        }
         try {
-            // TOML parser is soft and can parse even JSON strings, this additional check prevents it.
-            if (configText.trim()[0] === '{') {
-                throw new UserError(
-                    ErrorCode.BAD_CONFIG,
-                    `The config supplied: ${configText} is an invalid TOML and cannot be parsed`,
-                );
-            }
             return toml.parse(configText);
         } catch (err) {
             const errMessage: string = err.toString();
+            
+
             throw new UserError(
                 ErrorCode.BAD_CONFIG,
                 `The config supplied: ${configText} is an invalid TOML and cannot be parsed: ${errMessage}`,
