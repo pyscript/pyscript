6095571e5c71ec8525c4bbfd35361f3dc233d409
Madhur Tandon <madhurtandon23@gmail.com>
2023-03-23 17:07:11 +0530

    linting

--- pyscriptjs/jest-environment-jsdom.js
+++ pyscriptjs/jest-environment-jsdom.js
@@ -6,24 +6,23 @@ const { MessageChannel } = require('node:worker_threads');
 const { default: $JSDOMEnvironment, TestEnvironment } = require('jest-environment-jsdom');
 
 Object.defineProperty(exports, '__esModule', {
-  value: true
+    value: true,
 });
 
 class JSDOMEnvironment extends $JSDOMEnvironment {
-  constructor(...args) {
-    const {global} = super(...args);
-    if (!global.TextEncoder) {
-      global.TextEncoder = TextEncoder;
+    constructor(...args) {
+        const { global } = super(...args);
+        if (!global.TextEncoder) {
+            global.TextEncoder = TextEncoder;
+        }
+        if (!global.TextDecoder) {
+            global.TextDecoder = TextDecoder;
+        }
+        if (!global.MessageChannel) {
+            global.MessageChannel = MessageChannel;
+        }
     }
-    if (!global.TextDecoder) {
-      global.TextDecoder = TextDecoder;
-    }
-    if (!global.MessageChannel) {
-      global.MessageChannel = MessageChannel;
-    }
-  }
 }
 
 exports.default = JSDOMEnvironment;
-exports.TestEnvironment = TestEnvironment === $JSDOMEnvironment ?
-                            JSDOMEnvironment : TestEnvironment;
+exports.TestEnvironment = TestEnvironment === $JSDOMEnvironment ? JSDOMEnvironment : TestEnvironment;
