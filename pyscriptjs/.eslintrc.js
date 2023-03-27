3b31f99408516cbf60e0349011b70511d75e4b42
Antonio Cuni <anto.cuni@gmail.com>
2023-03-27 12:04:04 +0200

    modify the eslint rules. We want to be able to use the 'any' type

--- pyscriptjs/.eslintrc.js
+++ pyscriptjs/.eslintrc.js
@@ -19,13 +19,18 @@ module.exports = {
     ignorePatterns: ['node_modules'],
     rules: {
         'no-prototype-builtins': 'error',
-        '@typescript-eslint/no-unused-vars': ['error', { args: 'none' }],
-        '@typescript-eslint/no-explicit-any': 'error',
-        '@typescript-eslint/no-unsafe-assignment': 'error',
+        // we want to be able to use 'any', and to @ts-ignore certain
+        // lines. Disable the corresponding rules.
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
         '@typescript-eslint/no-unsafe-argument': 'error',
-        '@typescript-eslint/no-unsafe-member-access': 'error',
-        '@typescript-eslint/no-unsafe-call': 'error',
         '@typescript-eslint/no-unsafe-return': 'error',
+
+        // other rules
+        '@typescript-eslint/no-unused-vars': ['error', { args: 'none' }],
         '@typescript-eslint/no-floating-promises': 'error',
         '@typescript-eslint/restrict-plus-operands': 'error',
         '@typescript-eslint/no-empty-function': 'error',
