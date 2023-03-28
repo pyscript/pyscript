c75c742f4a2ffce9c19d285ca0fea74cdac40b7f
Hood Chatham <roberthoodchatham@gmail.com>
2023-03-27 19:21:33 -0700

    Python 3.11

--- pyscriptjs/tests/unit/setup.ts
+++ pyscriptjs/tests/unit/setup.ts
@@ -1,6 +1,5 @@
 import { jest } from '@jest/globals';
-import {readFileSync} from "fs";
-
+import { readFileSync } from 'fs';
 
 jest.unstable_mockModule('./python/pyscript/__init__.py', () => ({
     default: readFileSync('./src/python/pyscript/__init__.py'),
