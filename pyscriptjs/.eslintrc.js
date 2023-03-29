5c404cc9493b55585e3b1982792cb103ef9913b4
Antonio Cuni <anto.cuni@gmail.com>
2023-03-29 20:00:21 +0200

    it is perfectly fine to use any, shut up eslint

--- pyscriptjs/.eslintrc.js
+++ pyscriptjs/.eslintrc.js
@@ -22,12 +22,12 @@ module.exports = {
         '@typescript-eslint/ban-ts-comment': 'off',
 
         // any related lints
-        '@typescript-eslint/no-explicit-any': 'error',
-        '@typescript-eslint/no-unsafe-assignment': 'error',
-        '@typescript-eslint/no-unsafe-call': 'error',
-        '@typescript-eslint/no-unsafe-member-access': 'error',
-        '@typescript-eslint/no-unsafe-argument': 'error',
-        '@typescript-eslint/no-unsafe-return': 'error',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/no-unsafe-argument': 'off',
+        '@typescript-eslint/no-unsafe-return': 'off',
 
         // other rules
         'no-prototype-builtins': 'error',
