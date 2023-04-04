6de43a43331918167d0015831faf18d7c8fccd2f
mariana <marianameireles@protonmail.com>
2023-04-04 11:24:30 +0200

    update and improve test

--- pyscriptjs/tests/integration/test_event_handler.py
+++ pyscriptjs/tests/integration/test_event_handler.py
@@ -2,7 +2,7 @@ from .support import PyScriptTest
 
 
 class TestEventHandler(PyScriptTest):
-    def test_when_decorated_func_with_click_event(self):
+    def test_decorator_click_event(self):
         self.pyscript_run(
             """
             <button id="foo_id">foo_button</button>
@@ -18,23 +18,23 @@ class TestEventHandler(PyScriptTest):
         console_text = self.console.all.lines
         assert "I've clicked [object HTMLButtonElement] with id foo_id" in console_text
 
-    def test_when_decorated_func_without_event(self):
+    def test_decorator_without_event(self):
         self.pyscript_run(
             """
             <button id="foo_id">foo_button</button>
             <py-script>
                 from pyscript import when
-                @when("click", id="foo_id")
+                @when(id="foo_id")
                 def foo():
-                    print("A button was clicked")
+                    print("This will fail")
             </py-script>
         """
         )
         self.page.locator("text=foo_button").click()
         console_text = self.console.all.lines
-        assert "A button was clicked" in console_text
+        assert "This will fail" not in console_text
 
-    def test_multiple_when_uses_with_click_event(self):
+    def test_multiple_decorators_click_event(self):
         self.pyscript_run(
             """
             <button id="foo_id">foo_button</button>
@@ -58,12 +58,11 @@ class TestEventHandler(PyScriptTest):
         console_text = self.console.all.lines
         assert "I've clicked [object HTMLButtonElement] with id bar_id" in console_text
 
-    def test_py_event_without_decorator(self):
+    def test_py_click_no_decorator(self):
         self.pyscript_run(
             """
             <button py-click="reacts_to_py_click">no_when</button><br><br>
             <py-script>
-            from pyscript import when
             def reacts_to_py_click(evt):
                 print(f"Reacting to event {evt}")
             </py-script>
@@ -73,7 +72,7 @@ class TestEventHandler(PyScriptTest):
         console_text = self.console.all.lines
         assert "Reacting to event [object PointerEvent]" in console_text
 
-    def test_py_event_more_than_one_arg(self):
+    def test_py_click_two_args_no_decorator(self):
         self.pyscript_run(
             """
             <button py-click="multiple_args">two_args_button</button><br><br>
@@ -85,10 +84,9 @@ class TestEventHandler(PyScriptTest):
         )
         self.page.locator("text=two_args_button").click()
         tb_lines = self.console.error.lines[-1].splitlines()
-        assert tb_lines[0] == "[pyexec] Non-python exception:"
-        assert tb_lines[1] == "UserError: (PY0000): py-events take 0 or 1 arguments"
+        assert tb_lines[1] == "UserError: (PY0000): 'py-[event]' take 0 or 1 arguments"
 
-    def test_instance_method_when_with_click_event(self):
+    def test_py_click_method_no_decorator(self):
         self.pyscript_run(
             """
             <button py-click="instance.someEventFunc">instance_method</button>
@@ -104,24 +102,7 @@ class TestEventHandler(PyScriptTest):
         console_text = self.console.all.lines
         assert "Got event with target [object HTMLButtonElement]" in console_text
 
-    def test_instance_method_when_without_event(self):
-        self.pyscript_run(
-            """
-            <button py-click="instance.somefunc">instance_method</button>
-            <py-script>
-                class Instance():
-                    def somefunc(self):
-                        print("Somefunc got called")
-                instance = Instance()
-            </py-script>
-            """
-        )
-        self.page.locator("text=instance_method").click()
-        console_text = self.console.all.lines
-        assert "Somefunc got called" in console_text
-
-    # this needs to throw an err on py-event
-    def test_run_code_in_py_event_tag(self):
+    def test_run_code_in_py_click(self):
         self.pyscript_run(
             """
                 <button py-click="print('a hack!')">misuse_py_event</button>
@@ -129,15 +110,28 @@ class TestEventHandler(PyScriptTest):
         )
         self.page.locator("text=misuse_py_event").click()
         tb_lines = self.console.error.lines[-1].splitlines()
-        assert tb_lines[0] == "some err"
+        assert tb_lines[1] == "UserError: (PY0000): The code provided to 'py-[event]' should be the name of a function or Callable. To run an expression as code, use 'py-[event]-code'"
 
-    # this needs to run
-    def test_run_code_in_py_event_code_tag(self):
+    def test_run_code_in_py_click_code(self):
         self.pyscript_run(
             """
-                <button py-click="print('this is fine')">misuse_py_event</button>
+                <button py-click-code="print('printed code from code tag')">correct_use_py_event_code</button>
             """
         )
-        self.page.locator("text=misuse_py_event").click()
+        self.page.locator("text=correct_use_py_event_code").click()
         console_text = self.console.all.lines
-        assert "this is fine" in console_text
+        assert "printed code from code tag" in console_text
+
+    def test_run_callable_in_py_click_code(self):
+        self.pyscript_run(
+            """
+                <button id="foo" py-click-code="callable_fun">misuse_py_event_code</button>
+                <py-script>
+                    def callable_fun():
+                        print('This should fail')
+                </py-script>
+            """
+            )
+        self.page.locator("text=misuse_py_event").click()
+        tb_lines = self.console.error.lines[-1].splitlines()
+        assert tb_lines[1] == "UserError: (PY0000): The code provided to 'py-[event]-code' was the name of a Callable. Did you mean to use 'py-[event]?"
