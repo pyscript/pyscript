c44fb6fa73adbcdd5e31f3fe96c4c193f4431b6e
woxtu <woxtup@gmail.com>
2023-05-05 04:15:11 +0900

    Fix an error message when loading local modules failed

--- pyscriptjs/tests/unit/fetch.test.ts
+++ pyscriptjs/tests/unit/fetch.test.ts
@@ -94,7 +94,7 @@ describe('robustFetch', () => {
         const expectedError = new FetchError(
             ErrorCode.FETCH_ERROR,
             `PyScript: Access to local files
-        (using "files" in &lt;py-config&gt;)
+        (using [[fetch]] configurations in &lt;py-config&gt;)
         is not available when directly opening a HTML file;
         you must use a webserver to serve the additional files.
         See <a style="text-decoration: underline;" href="https://github.com/pyscript/pyscript/issues/257#issuecomment-1119595062">this reference</a>
