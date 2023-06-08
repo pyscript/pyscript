50b1a1d7c5e46f7baa10ce8d3ddfefc491f7406e
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2023-06-08 17:39:01 +0200

    Fix #1512 - Improve worker tests + update Pyodide (#1513)
    * Fix #1512 - Improve worker tests + update Pyodide
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- pyscript.core/esm/runtime/pyodide.js
+++ pyscript.core/esm/runtime/pyodide.js
@@ -14,11 +14,12 @@ const type = "pyodide";
 /* c8 ignore start */
 export default {
     type: [type, "py"],
-    module: (version = "0.22.1") =>
+    module: (version = "0.23.2") =>
         `https://cdn.jsdelivr.net/pyodide/v${version}/full/pyodide.mjs`,
-    async engine({ loadPyodide }, config) {
+    async engine({ loadPyodide }, config, url) {
         const { stderr, stdout, get } = stdio();
-        const runtime = await get(loadPyodide({ stderr, stdout }));
+        const indexURL = url.slice(0, url.lastIndexOf("/"));
+        const runtime = await get(loadPyodide({ stderr, stdout, indexURL }));
         if (config.fetch) await fetchPaths(this, runtime, config.fetch);
         if (config.packages) {
             await runtime.loadPackage("micropip");
--- pyscript.core/test/worker/worker.py
+++ pyscript.core/test/worker/worker.py
@@ -1,9 +1,12 @@
+import re
+
 from js import xworker
 
 
 def on_message(event):
     print(event.data)
-    xworker.postMessage("Pyodide: Hello MicroPython 👋")
+    foreign = re.search("^[^:]+", event.data).group(0)
+    xworker.postMessage("Python: Hello " + foreign + " 👋")
 
 
 xworker.onmessage = on_message
