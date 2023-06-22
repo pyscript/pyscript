118add1bb88b68a4e490cc32ad5f468406c4680a
Nicholas H.Tollervey <ntoll@ntoll.org>
2023-06-22 12:49:09 +0100

    Bump micropython to version 1.20.0-253.

--- pyscript.core/esm/interpreter/micropython.js
+++ pyscript.core/esm/interpreter/micropython.js
@@ -7,7 +7,7 @@ const type = "micropython";
 /* c8 ignore start */
 export default {
     type,
-    module: (version = "1.20.0-239") =>
+    module: (version = "1.20.0-253") =>
         `https://cdn.jsdelivr.net/npm/@micropython/micropython-webassembly-pyscript@${version}/micropython.mjs`,
     async engine({ loadMicroPython }, config, url) {
         const { stderr, stdout, get } = stdio();
