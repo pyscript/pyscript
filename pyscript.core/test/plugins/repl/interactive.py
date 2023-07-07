8d557f82ac1dc8a03a66f55685e2a501055f2fb0
Jeff Glass <glass.jeffrey@gmail.com>
2023-07-07 13:53:27 -0500

    Tweak demos

--- pyscript.core/test/plugins/repl/interactive.py
+++ pyscript.core/test/plugins/repl/interactive.py
@@ -1,8 +1,6 @@
-from code import InteractiveConsole
 import sys
+from code import InteractiveConsole
 
-import js
-from pyodide.ffi import to_js
 
 class xtermInteractive(InteractiveConsole):
     def __init__(self, term, locals=None, filename="<console>"):
@@ -18,9 +16,9 @@ class xtermInteractive(InteractiveConsole):
         # Makes this object file-like to act as sys.stdout and sys.stderr
         pass
 
-    def beginInteraction(self, banner=None, exitmsg = None):
+    def beginInteraction(self, banner=None, exitmsg=None):
         try:
-            sys.ps1    
+            sys.ps1
         except AttributeError:
             sys.ps1 = ">>> "
         try:
@@ -30,8 +28,7 @@ class xtermInteractive(InteractiveConsole):
 
         cprt = 'Type "help", "copyright", "credits" or "license" for more information.'
         if banner is None:
-            self.write("Python %s on %s\n%s\n" %
-                    (sys.version, sys.platform, cprt))
+            self.write("Python %s on %s\n%s\n" % (sys.version, sys.platform, cprt))
         elif banner:
             self.write(f"{str(banner)}")
 
@@ -42,26 +39,29 @@ class xtermInteractive(InteractiveConsole):
         self.write(sys.ps1)
 
     def endInteraction(self):
-        self.write("The REPL cannot be killed") #Todo make this functional
+        self.write("The REPL cannot be killed")  # Todo make this functional
 
-    #onKey(e: {key: string, domEvent: KeyboardEvent}, f: void)
+    # onKey(e: {key: string, domEvent: KeyboardEvent}, f: void)
     def onKey(self, event, f):
-        #js.console.log(f"Got key {event.key}")
-        #js.console.log(to_js(event.domEvent))
-        if event.key == '\r': #Enter
+        # js.console.log(f"Got key {event.key}")
+        # js.console.log(to_js(event.domEvent))
+        if event.key == "\r":  # Enter
             self.write("\n")
             self.more = self.push(self.line)
-            self.line = ''
-            if self.more: self.write(sys.ps2)
-            else: self.write(sys.ps1)
-        elif event.domEvent.ctrlKey and event.domEvent.key == 'd':
+            self.line = ""
+            if self.more:
+                self.write(sys.ps2)
+            else:
+                self.write(sys.ps1)
+        elif event.domEvent.ctrlKey and event.domEvent.key == "d":
             self.endInteraction()
-        elif event.domEvent.key == 'Backspace':
+        elif event.domEvent.key == "Backspace":
             if len(self.line):
                 self.line = self.line[:-1]
-                self.write('\x1b[D \x1b[D')
+                self.write("\x1b[D \x1b[D")
         else:
-            if len(event.key) == 1: self.line += event.key
+            if len(event.key) == 1:
+                self.line += event.key
             self.write(event.key)
 
     def runcode(self, code):
@@ -75,17 +75,18 @@ class xtermInteractive(InteractiveConsole):
         elsewhere in this code, and may not always be caught.  The
         caller should be prepared to deal with it.
 
-        This version is slightly modified from 
-        InteractiveInterpreter.runcode(), in that it runs with the 
+        This version is slightly modified from
+        InteractiveInterpreter.runcode(), in that it runs with the
         Pyodide global scope. It is used by runsource(), which is used
         by push()
 
         """
         try:
-            exec(code, globals())
+            exec(code, globals(), locals())
         except SystemExit:
             raise
         except:
             self.showtraceback()
+
 
 xtermInteractive
