c96f5912dfadfb05935e3a9cdc8b70a6a7ba84be
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2023-08-03 11:22:16 +0200

    Prepared for the first publish (#1620)

--- pyscript.next/src/core.js
+++ pyscript.next/src/core.js
@@ -193,6 +193,12 @@ class PyScriptElement extends HTMLElement {
 
 customElements.define("py-script", PyScriptElement);
 
+/**
+ * A `Worker` facade able to bootstrap on the worker thread only a PyScript module.
+ * @param {string} file the python file to run ina worker.
+ * @param {{config?: string | object, async?: boolean}} [options] optional configuration for the worker.
+ * @returns {Worker & {sync: ProxyHandler<object>}}
+ */
 export function PyWorker(file, options) {
     // this propagates pyscript worker hooks without needing a pyscript
     // bootstrap + it passes arguments and enforces `pyodide`
