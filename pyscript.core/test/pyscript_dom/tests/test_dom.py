fef9dfc3a77bc57d0fcd447bcca55465815fff17
Fabio Pliger <fpliger@anaconda.com>
2023-09-11 17:47:38 -0500

    fix remaining tests

--- pyscript.core/test/pyscript_dom/tests/test_dom.py
+++ pyscript.core/test/pyscript_dom/tests/test_dom.py
@@ -37,73 +37,84 @@ def test_getitem_by_id():
 
 
 def test_getitem_by_class():
-    id_ = "test_class_selector"
+    ids = [
+        "test_class_selector",
+        "test_selector_w_children",
+        "test_selector_w_children_child_1",
+    ]
     expected_class = "a-test-class"
     result = pydom[f".{expected_class}"]
     div = result[0]
 
-    # EXPECT to find exact number of elements with the class in the page (== 1)
-    assert len(result) == 1
-    # EXPECT the element id to match what we expect
-    assert div.id == id_
+    # EXPECT to find exact number of elements with the class in the page (== 3)
+    assert len(result) == 3
+
+    # EXPECT that all element ids are in the expected list
+    assert [el.id for el in result] == ids
 
 
 def test_read_n_write_collection_elements():
-    elements = pydom[".multi-elem-div"]
+    elements = pydom[".multi-elems"]
 
     # ids = [f"#test_rr_input_{x}" for x in ["txt", "btn", "email", "password"]]
     # values = el.read_elements(ids)
     for element in elements:
-        assert element.text == f"Content {element.id.replace('#', '')}"
+        assert element.html == f"Content {element.id.replace('#', '')}"
 
     new_content = "New Content"
-    elements.text = new_content
+    elements.html = new_content
     for element in elements:
-        assert element.text == new_content
+        assert element.html == new_content
 
 
 class TestElement:
     def test_query(self):
         # GIVEN an existing element on the page, with at least 1 child element
         id_ = "test_selector_w_children"
-        parent_div = el.query(f"#{id_}")
+        parent_div = pydom[f"#{id_}"][0]
 
         # EXPECT it to be able to query for the first child element
-        div = parent_div.query("div")
+        div = parent_div.find("div")[0]
 
         # EXPECT the new element to be associated with the parent
         assert div.parent == parent_div
-        # EXPECT the new element to be a CoreElement
-        assert isinstance(div, el.CoreElement)
+        # EXPECT the new element to be a BaseElement
+        assert isinstance(div, pydom.BaseElement)
         # EXPECT the div attributes to be == to how they are configured in the page
-        assert div.innerHTML == "Child 1"
+        assert div.html == "Child 1"
         assert div.id == "test_selector_w_children_child_1"
 
     def test_equality(self):
         # GIVEN 2 different Elements pointing to the same underlying element
         id_ = "test_id_selector"
-        div = el.query(f"#{id_}")
-        div2 = el.query(f"#{id_}")
+        selector = f"#{id_}"
+        div = pydom[selector][0]
+        div2 = pydom[selector][0]
+
         # EXPECT them to be equal
         assert div == div2
+        # EXPECT them to be different objects
+        assert div is not div2
+
         # EXPECT their value to always be equal
-        assert div.value == div2.value
-        div.value = "foo"
-        assert div.value == div2.value
+        assert div.html == div2.html
+        div.html = "some value"
+
+        assert div.value == div2.value == "some value"
 
     def test_read_classes(self):
         id_ = "test_class_selector"
         expected_class = "a-test-class"
-        div = el.query(f"#{id_}")
+        div = pydom[f"#{id_}"][0]
         assert div.classes == [expected_class]
 
     def test_add_remove_class(self):
         id_ = "div-no-classes"
         classname = "tester-class"
-        div = el.query(f"#{id_}")
+        div = pydom[f"#{id_}"][0]
         assert not div.classes
         div.add_class(classname)
-        same_div = el.query(f"#{id_}")
+        same_div = pydom[f"#{id_}"][0]
         assert div.classes == [classname] == same_div.classes
         div.remove_class(classname)
         assert div.classes == [] == same_div.classes
