4d89cbde015bf22d26e40f08e20353d31f58a0a4
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2023-09-14 18:11:10 +0200

    [next] Terminal smoke-test follow up (#1702)

--- pyscript.core/test/terminal.py
+++ pyscript.core/test/terminal.py
@@ -1,22 +1,13 @@
 ###### magic monkey patching ######
 import sys
 import builtins
-import js
 from pyscript import sync
 from pyodide.code import eval_code
 
 sys.stdout = sync
 builtins.input = sync.readline
 
-globals = {"js": js}
-
 ####### main code ######
-while True:
-    code = input(f"> ")
-    if len(code):
-        try:
-            result = eval_code(f"{code}", globals=globals)
-            if result is not None:
-                print(result)
-        except:
-            print(f"Unable to evaluate: {code}")
+import code
+
+code.interact()
