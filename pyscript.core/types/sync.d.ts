a211c0ff27734d80b29e9f010d17b22bae4c17f7
Jeff Glass <glass.jeffrey@gmail.com>
2023-09-22 07:35:47 -0500

    Add sync typings

--- pyscript.core/types/sync.d.ts
+++ pyscript.core/types/sync.d.ts
@@ -1,4 +1,9 @@
 declare namespace _default {
-    function sleep(seconds: any): Promise<any>;
+    /**
+     * 'Sleep' for the given number of seconds. Used to implement Python's time.sleep in Worker threads.
+     * @param {number} seconds The number of seconds to sleep.
+     * @returns {undefined}
+     */
+    function sleep(seconds: number): undefined;
 }
 export default _default;
