07d919409becbda01f04af4cd015c6913da057c9
Jeff Glass <glass.jeffrey@gmail.com>
2023-09-22 07:40:05 -0500

    Adjust typings in config

--- pyscript.core/src/config.js
+++ pyscript.core/src/config.js
@@ -46,16 +46,14 @@ const syntaxError = (type, url, { message }) => {
 };
 
 // find the shared config for all py-script elements
-let config;
+let config, type;
 
-/** @type {Promise<any> | undefined} A Promise wrapping any plugins which should be loaded */
+/** @type {Promise<any> | undefined} A Promise wrapping any plugins which should be loaded. */
 let plugins;
-/** @type {any} The configuration parsed as a JSON object*. May be any of the return types of JSON.parse() ( {number | string | boolean | null | object | Array}*/
+/** @type {any} The PyScript configuration parsed as a JSON object*. May be any of the return types of JSON.parse() ( {number | string | boolean | null | object | Array} */
 let parsed;
-/** @type {SyntaxError | undefined} The error thrown when parsing the config, if any.*/
+/** @type {SyntaxError | undefined} The error thrown when parsing the PyScript config, if any.*/
 let error;
-/** @type {string | null} The Custom Type or Interpreter Type specified in the config, if any*/
-let type;
 
 let pyConfig = $("py-config");
 if (pyConfig) {
--- pyscript.core/types/config.d.ts
+++ pyscript.core/types/config.d.ts
@@ -1,7 +1,7 @@
-/** @type {any} The configuration parsed as a JSON object or similar. (Return type of JSON.parse is something like {number | string | boolean | null | object | Array}; TS by default gives up and says 'any')*/
+/** @type {any} The PyScript configuration parsed as a JSON object*. May be any of the return types of JSON.parse() ( {number | string | boolean | null | object | Array} */
 declare let parsed: any;
-/** @type {Promise<any> | undefined} A Promise wrapping any plugins which should be loaded */
+/** @type {Promise<any> | undefined} A Promise wrapping any plugins which should be loaded. */
 export let plugins: Promise<any> | undefined;
-/** @type {SyntaxError | undefined} The error thrown when parsing the config, if any.*/
+/** @type {SyntaxError | undefined} The error thrown when parsing the PyScript config, if any.*/
 export let error: SyntaxError | undefined;
 export { parsed as config };
