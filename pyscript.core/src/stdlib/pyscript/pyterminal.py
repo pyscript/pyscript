8c64d421898db26c7cb6dca616af7a4d95de9373
Antonio Cuni <anto.cuni@gmail.com>
2023-10-10 16:57:02 +0200

    missing file

---
+++ pyscript.core/src/stdlib/pyscript/pyterminal.py
@@ -0,0 +1,24 @@
+import builtins
+import sys
+
+import js
+
+from pyscript import sync
+
+
+class PyTerminal:
+    def write(self, line):
+        sync.pyterminal_write(line)
+
+    def input(self, prompt):
+        return sync.pyterminal_readline(prompt)
+
+
+PY_TERMINAL = None
+
+
+def init():
+    global PY_TERMINAL
+    PY_TERMINAL = PyTerminal()
+    sys.stdout = sys.stderr = PY_TERMINAL
+    builtins.input = PY_TERMINAL.input
