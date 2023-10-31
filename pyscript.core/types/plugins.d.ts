72f266532bb906574baabc4605618f5607027030
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2023-10-31 15:16:15 +0100

    PyScript Terminal - the latest kind (#1816)

--- pyscript.core/types/plugins.d.ts
+++ pyscript.core/types/plugins.d.ts
@@ -1,4 +1,5 @@
-declare namespace _default {
-    function error(): Promise<typeof import("./plugins/error.js")>;
-}
+declare const _default: {
+    error: () => Promise<typeof import("./plugins/error.js")>;
+    "py-terminal": () => Promise<typeof import("./plugins/py-terminal.js")>;
+};
 export default _default;
