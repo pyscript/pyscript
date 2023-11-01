e81830a2ea7775edd5287427a89c7ac9e5b33b40
Fabio Pliger <fpliger@users.noreply.github.com>
2023-11-01 10:33:38 -0500

    Value property to PyDom.Element and ElementCollection (#1828)
    * add base test for input value field
    
    * add value property to Element
    
    * add test for non supported element
    
    * prevent users to set value attribute on elements that do not support it
    
    * add test for setting value on collections
    
    * add value property to collection and add more tests
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- pyscript.core/types/stdlib/pyscript.d.ts
+++ pyscript.core/types/stdlib/pyscript.d.ts
@@ -1,13 +1,14 @@
-declare namespace _default {
-    let pyscript: {
+declare const _default: {
+    pyscript: {
         "__init__.py": string;
         "display.py": string;
         "event_handling.py": string;
         "magic_js.py": string;
         "util.py": string;
     };
-    let pyweb: {
+    "pyscript.py": string;
+    pyweb: {
         "pydom.py": string;
     };
-}
+};
 export default _default;
