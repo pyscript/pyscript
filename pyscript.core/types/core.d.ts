f1a46be73834dc4d9d6efd768c0055bb905f0ba5
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2023-11-07 17:17:40 +0100

    Fix #1838 - Provides all TS from all projects (#1843)

--- pyscript.core/types/core.d.ts
+++ pyscript.core/types/core.d.ts
@@ -1,6 +1,42 @@
 import TYPES from "./types.js";
-declare const exportedPyWorker: any;
-declare const exportedHooks: any;
-declare const exportedConfig: any;
-declare const exportedWhenDefined: any;
+/**
+ * A `Worker` facade able to bootstrap on the worker thread only a PyScript module.
+ * @param {string} file the python file to run ina worker.
+ * @param {{config?: string | object, async?: boolean}} [options] optional configuration for the worker.
+ * @returns {Worker & {sync: ProxyHandler<object>}}
+ */
+declare function exportedPyWorker(file: string, options?: {
+    config?: string | object;
+    async?: boolean;
+}): Worker & {
+    sync: ProxyHandler<object>;
+};
+declare const exportedHooks: {
+    main: {
+        onWorker: Set<Function>;
+        onReady: Set<Function>;
+        onBeforeRun: Set<Function>;
+        onBeforeRunAsync: Set<Function>;
+        onAfterRun: Set<Function>;
+        onAfterRunAsync: Set<Function>;
+        codeBeforeRun: Set<string>;
+        codeBeforeRunAsync: Set<string>;
+        codeAfterRun: Set<string>;
+        codeAfterRunAsync: Set<string>;
+    };
+    worker: {
+        onReady: Set<Function>;
+        onBeforeRun: Set<Function>;
+        onBeforeRunAsync: Set<Function>;
+        onAfterRun: Set<Function>;
+        onAfterRunAsync: Set<Function>;
+        codeBeforeRun: Set<string>;
+        codeBeforeRunAsync: Set<string>;
+        codeAfterRun: Set<string>;
+        codeAfterRunAsync: Set<string>;
+    };
+};
+declare const exportedConfig: {};
+declare const exportedWhenDefined: (type: string) => Promise<any>;
+import sync from "./sync.js";
 export { TYPES, exportedPyWorker as PyWorker, exportedHooks as hooks, exportedConfig as config, exportedWhenDefined as whenDefined };
