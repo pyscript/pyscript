5d666d4789059ee510af0a426b04b047b1d68209
Fabio Pliger <fpliger@anaconda.com>
2023-11-07 16:43:34 -0600

    add deprecation manager to take care of showing a notification in case script src is being loaded from latest

---
+++ pyscript.core/src/plugins/deprecations-manager.js
@@ -0,0 +1,21 @@
+// PyScript Derepcations Plugin
+import { hooks } from "../core.js";
+import { notify } from "./error.js";
+
+// eslint-disable-next-line no-unused-vars
+hooks.main.onReady.add(function checkDeprecations(pyScript) {
+    const scripts = document.querySelectorAll("script");
+    for (const script of scripts)
+        checkLoadingScriptsFromLatest(script.src);
+});
+// eslint-disable-next-line no-unused-vars
+
+/**
+ * Check if src being loaded from pyscript.net/latest and display a notification if true
+ * * @param {string} src
+ */
+function checkLoadingScriptsFromLatest(src){
+    if (/\/pyscript\.net\/latest/.test(src)){
+        notify("Loading scripts from latest is deprecated and will be removed soon. Please use a specific version instead.");
+    }
+}
