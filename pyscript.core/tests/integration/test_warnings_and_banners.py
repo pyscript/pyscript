9df908a159718c5b65e316c40febd05d478adb08
Fabio Pliger <fpliger@anaconda.com>
2023-11-09 23:07:47 -0600

    add a wait selector when testing banner since worker seems to take too long to render in CI

--- pyscript.core/tests/integration/test_warnings_and_banners.py
+++ pyscript.core/tests/integration/test_warnings_and_banners.py
@@ -2,7 +2,6 @@ import pytest
 
 from .support import PyScriptTest, skip_worker
 
-
 class TestWarningsAndBanners(PyScriptTest):
     # Test the behavior of generated warning banners
 
@@ -17,14 +16,17 @@ class TestWarningsAndBanners(PyScriptTest):
             extra_pre_head='<script type="module" src="https://pyscript.net/latest/core.js"></script>',
         )
 
-        loc = self.page.locator(".py-error")
-
-        # Only one banner should appear
-        assert loc.count() == 1
+        # wait for the banner to appear (we could have a page.locater call but for some reason
+        # the worker takes to long to render on CI, since it's a test we can afford 2 calls)
+        loc = self.page.wait_for_selector(".py-error")
         assert (
-            loc.text_content()
+            loc.inner_text()
             == "Loading scripts from latest is deprecated and will be removed soon. Please use a specific version instead."
         )
+
+        # Only one banner should appear
+        loc = self.page.locator(".py-error")
+        assert loc.count() == 1
 
     @pytest.mark.skip("NEXT: To check if behaviour is consistent with classic")
     def test_create_singular_warning(self):
