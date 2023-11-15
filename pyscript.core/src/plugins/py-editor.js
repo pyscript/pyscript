515e9254c9a1afe1c6d293d902547c13cbcab9e9
Fabio Pliger <fpliger@anaconda.com>
2023-11-15 08:22:27 -0600

    replace py-terminal custom code with py-editor code

--- pyscript.core/src/plugins/py-editor.js
+++ pyscript.core/src/plugins/py-editor.js
@@ -1,10 +1,10 @@
-// PyScript py-terminal plugin
+// PyScript py-editor plugin
 import { TYPES, hooks } from "../core.js";
 import { notify } from "./error.js";
 
-const SELECTOR = [...TYPES.keys()]
-    .map((type) => `script[type="${type}"][terminal],${type}-script[terminal]`)
-    .join(",");
+
+const SELECTOR = 'script[type="py-editor"]';
+
 
 // show the error on main and
 // stops the module from keep executing
@@ -13,25 +13,23 @@ const notifyAndThrow = (message) => {
     throw new Error(message);
 };
 
-const pyTerminal = async () => {
-    const terminals = document.querySelectorAll(SELECTOR);
+const pyEditor = async () => {
+    const editors = document.querySelectorAll(SELECTOR);
 
     // no results will look further for runtime nodes
-    if (!terminals.length) return;
+    if (!editors.length) return;
 
     // if we arrived this far, let's drop the MutationObserver
     // as we only support one terminal per page (right now).
     mo.disconnect();
 
-    // we currently support only one terminal as in "classic"
-    if (terminals.length > 1) notifyAndThrow("You can use at most 1 terminal.");
-
-    const [element] = terminals;
+    const [element] = editors;
     // hopefully to be removed in the near future!
     if (element.matches('script[type="mpy"],mpy-script'))
         notifyAndThrow("Unsupported terminal.");
 
     // import styles lazily
+    // TODO: Replace all CSS below
     document.head.append(
         Object.assign(document.createElement("link"), {
             rel: "stylesheet",
@@ -40,16 +38,17 @@ const pyTerminal = async () => {
     );
 
     // lazy load these only when a valid terminal is found
+    // TODO: Replace all JS below
     const [{ Terminal }, { Readline }] = await Promise.all([
         import(/* webpackIgnore: true */ "../3rd-party/xterm.js"),
         import(/* webpackIgnore: true */ "../3rd-party/xterm-readline.js"),
     ]);
 
-    const readline = new Readline();
 
     // common main thread initialization for both worker
     // or main case, bootstrapping the terminal on its target
     const init = (options) => {
+        
         let target = element;
         const selector = element.getAttribute("target");
         if (selector) {
@@ -58,89 +57,18 @@ const pyTerminal = async () => {
                 document.querySelector(selector);
             if (!target) throw new Error(`Unknown target ${selector}`);
         } else {
-            target = document.createElement("py-terminal");
+            target = document.createElement("py-editor");
             target.style.display = "block";
             element.after(target);
         }
-        const terminal = new Terminal({
-            theme: {
-                background: "#191A19",
-                foreground: "#F5F2E7",
-            },
-            ...options,
-        });
-        terminal.loadAddon(readline);
-        terminal.open(target);
-        terminal.focus();
+        
     };
 
-    // branch logic for the worker
-    if (element.hasAttribute("worker")) {
-        // when the remote thread onReady triggers:
-        // setup the interpreter stdout and stderr
-        const workerReady = ({ interpreter }, { sync }) => {
-            sync.pyterminal_drop_hooks();
-            const decoder = new TextDecoder();
-            let data = "";
-            const generic = {
-                isatty: true,
-                write(buffer) {
-                    data = decoder.decode(buffer);
-                    sync.pyterminal_write(data);
-                    return buffer.length;
-                },
-            };
-            interpreter.setStdout(generic);
-            interpreter.setStderr(generic);
-            interpreter.setStdin({
-                isatty: true,
-                stdin: () => sync.pyterminal_read(data),
-            });
-        };
-
-        // add a hook on the main thread to setup all sync helpers
-        // also bootstrapping the XTerm target on main
-        hooks.main.onWorker.add(function worker(_, xworker) {
-            hooks.main.onWorker.delete(worker);
-            init({
-                disableStdin: false,
-                cursorBlink: true,
-                cursorStyle: "block",
-            });
-            xworker.sync.pyterminal_read = readline.read.bind(readline);
-            xworker.sync.pyterminal_write = readline.write.bind(readline);
-            // allow a worker to drop main thread hooks ASAP
-            xworker.sync.pyterminal_drop_hooks = () => {
-                hooks.worker.onReady.delete(workerReady);
-            };
-        });
-
-        // setup remote thread JS/Python code for whenever the
-        // worker is ready to become a terminal
-        hooks.worker.onReady.add(workerReady);
-    } else {
-        // in the main case, just bootstrap XTerm without
-        // allowing any input as that's not possible / awkward
-        hooks.main.onReady.add(function main({ io }) {
-            console.warn("py-terminal is read only on main thread");
-            hooks.main.onReady.delete(main);
-            init({
-                disableStdin: true,
-                cursorBlink: false,
-                cursorStyle: "underline",
-            });
-            io.stdout = (value) => {
-                readline.write(`${value}\n`);
-            };
-            io.stderr = (error) => {
-                readline.write(`${error.message || error}\n`);
-            };
-        });
-    }
+   
 };
 
-const mo = new MutationObserver(pyTerminal);
+const mo = new MutationObserver(pyEditor);
 mo.observe(document, { childList: true, subtree: true });
 
 // try to check the current document ASAP
-export default pyTerminal();
+export default pyEditor();
