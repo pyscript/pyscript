069e4bb267a97a9be7a5817dd182cc229179ccee
webreflection <andrea.giammarchi@gmail.com>
2023-12-06 10:14:12 +0100

    Amend #1880 - Bootstrap pyscript module ASAP

--- pyscript.core/test/worker.py
+++ pyscript.core/test/worker.py
@@ -1,12 +1,12 @@
+import js
+js.document.body.append("document patch ")
+
 import a
-from pyscript import display, sync
+from pyscript import display, sync, RUNNING_IN_WORKER
 
 display("Hello World", target="test", append=True)
 
+print(RUNNING_IN_WORKER)
 print("sleeping")
 sync.sleep(1)
 print("awake")
-
-import js
-
-js.document.body.append("document patch ")
