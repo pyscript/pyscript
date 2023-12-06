96e671b55fda63a73940eb7e9d5f280ec71d2c46
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2023-12-06 11:16:05 +0100

    Amend #1880 - Bootstrap pyscript module ASAP (#1888)
      * bootstrap *pyscript* module ASAP as stdlib
      * improve the test to be sure `js.document` exists **before** our module is imported

--- pyscript.core/test/worker.py
+++ pyscript.core/test/worker.py
@@ -1,12 +1,13 @@
-import a
-from pyscript import display, sync
-
-display("Hello World", target="test", append=True)
-
-print("sleeping")
-sync.sleep(1)
-print("awake")
-
 import js
 
 js.document.body.append("document patch ")
+
+import a
+from pyscript import RUNNING_IN_WORKER, display, sync
+
+display("Hello World", target="test", append=True)
+
+print(RUNNING_IN_WORKER)
+print("sleeping")
+sync.sleep(1)
+print("awake")
