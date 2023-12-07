f2a8fbc36e320696bd878506cdb0e1f44c46db54
Fabio Pliger <fpliger@anaconda.com>
2023-12-06 18:19:47 -0600

    add test for text attribute

--- pyscript.core/test/pyscript_dom/tests/test_dom.py
+++ pyscript.core/test/pyscript_dom/tests/test_dom.py
@@ -173,7 +173,19 @@ class TestElement:
         # EXPECT the element html and underlying JS Element innerHTML property
         # to match what we expect and what
         assert div.html == div._js.innerHTML == "<b>New Content</b>"
-        # assert div.text == div._js.textContent == "New Content"
+        assert div.text == div._js.textContent == "New Content"
+        
+    def test_text_attribute(self):
+        # GIVEN an existing element on the page with a known empty text content
+        div = pydom["#element_attribute_tests"][0]
+
+        # WHEN we set the html attribute
+        div.text = "<b>New Content</b>"
+        
+        # EXPECT the element html and underlying JS Element innerHTML property
+        # to match what we expect and what
+        assert div.html == div._js.innerHTML == "<b>New Content</b>"
+        assert div.text == div._js.textContent == "<b>New Content</b>"
 
 
 class TestCollection:
