645de6649969f218de3c33b4787ce8dc555d774d
Fabio Pliger <fpliger@anaconda.com>
2024-01-25 09:22:02 -0600

    add docstrings to camera example

--- pyscript.core/test/camera.py
+++ pyscript.core/test/camera.py
@@ -8,6 +8,7 @@ devices = {}
 
 
 async def list_media_devices(event=None):
+    """List the available media devices."""
     global devices
     for i, device in enumerate(await media.list_devices()):
         devices[device.id] = device
@@ -17,13 +18,14 @@ async def list_media_devices(event=None):
 
 @when("click", "#pick-device")
 async def connect_to_device(e):
+    """Connect to the selected device."""
     device = devices[devicesSelect.value]
     video._js.srcObject = await device.get_stream()
 
 
 @when("click", "#snap")
 async def camera_click(e):
+    """Take a picture and download it."""
     video.snap().download()
-
 
 await list_media_devices()
