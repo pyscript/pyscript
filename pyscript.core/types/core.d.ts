181d276c8bc072e8bbc098214bf76034520ead3f
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2024-02-02 15:03:30 +0100

    Allow Workers w/out SharedArrayBuffer (#1967)
    Allow Workers w/out SharedArrayBuffer

--- pyscript.core/types/core.d.ts
+++ pyscript.core/types/core.d.ts
@@ -3,14 +3,26 @@ import TYPES from "./types.js";
  * A `Worker` facade able to bootstrap on the worker thread only a PyScript module.
  * @param {string} file the python file to run ina worker.
  * @param {{config?: string | object, async?: boolean}} [options] optional configuration for the worker.
- * @returns {Worker & {sync: ProxyHandler<object>}}
+ * @returns {Promise<Worker & {sync: object}>}
  */
 declare function exportedPyWorker(file: string, options?: {
     config?: string | object;
     async?: boolean;
-}): Worker & {
-    sync: ProxyHandler<object>;
-};
+}): Promise<Worker & {
+    sync: object;
+}>;
+/**
+ * A `Worker` facade able to bootstrap on the worker thread only a PyScript module.
+ * @param {string} file the python file to run ina worker.
+ * @param {{config?: string | object, async?: boolean}} [options] optional configuration for the worker.
+ * @returns {Promise<Worker & {sync: object}>}
+ */
+declare function exportedMPWorker(file: string, options?: {
+    config?: string | object;
+    async?: boolean;
+}): Promise<Worker & {
+    sync: object;
+}>;
 declare const exportedHooks: {
     main: {
         onWorker: Set<Function>;
@@ -39,4 +51,4 @@ declare const exportedHooks: {
 declare const exportedConfig: {};
 declare const exportedWhenDefined: (type: string) => Promise<any>;
 import sync from "./sync.js";
-export { TYPES, exportedPyWorker as PyWorker, exportedHooks as hooks, exportedConfig as config, exportedWhenDefined as whenDefined };
+export { TYPES, exportedPyWorker as PyWorker, exportedMPWorker as MPWorker, exportedHooks as hooks, exportedConfig as config, exportedWhenDefined as whenDefined };
