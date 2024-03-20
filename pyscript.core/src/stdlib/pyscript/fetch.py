8626c546c4058782c046a3532a289cf3f13881a0
webreflection <andrea.giammarchi@gmail.com>
2024-03-20 10:19:28 +0100

    rolled back the direct utility idea

--- pyscript.core/src/stdlib/pyscript/fetch.py
+++ pyscript.core/src/stdlib/pyscript/fetch.py
@@ -1,37 +1,4 @@
-import json
-
-import js
-
-### wrap the response to grant Pythonic results via overrides
-class _Response:
-    def __init__(self, response):
-        self.response = response
-
-    # forward response.ok and others
-    def __getattr__(self, attr):
-        return getattr(self.response, attr)
-
-    # exposed methods with Pythonic results
-    def arrayBuffer(self):
-        return _array_buffer(self.response, "")
-
-    def blob(self):
-        return _blob(self.response, "")
-
-    def bytearray(self):
-        return _bytearray(self.response, "")
-
-    def json(self):
-        return _json(self.response, "")
-
-    def text(self):
-        return _text(self.response, "")
-
-
-### generic helpers
-def _awaited(fetch, response):
-    fetch._response = response
-    return _Response(response)
+import json, js
 
 def _as_bytearray(buffer):
     ui8a = js.Uint8Array.new(buffer)
@@ -41,68 +8,40 @@ def _as_bytearray(buffer):
         ba[i] = ui8a[i]
     return ba
 
-def _check(response, url):
-    if not response.ok:
-        msg = f"URL {url} failed with status {response.status}"
-        raise Exception(msg)
+### wrap the response to grant Pythonic results
+class _Response:
+    def __init__(self, response):
+        self._response = response
 
-async def _buffer(task, url):
-    response = await _response(task, url)
-    return await response.arrayBuffer()
+    # forward response.ok and others
+    def __getattr__(self, attr):
+        return getattr(self._response, attr)
 
-# return the response and optionally check if it's OK
-async def _response(task, url):
-    check = False
+    # exposed methods with Pythonic results
+    async def arrayBuffer(self):
+        buffer = await self._response.arrayBuffer()
+        # works in Pyodide
+        if hasattr(buffer, "to_py"):
+            return buffer.to_py()
+        # shims in MicroPython
+        return memoryview(_as_bytearray(buffer))
 
-    if len(url):
-        if task._response is None:
-            check = True
-            task = await task
-        else:
-            task = task._response
+    async def blob(self):
+        return await self._response.blob()
 
-    if check:
-        _check(task, url)
+    async def bytearray(self):
+        buffer = await self._response.arrayBuffer()
+        return _as_bytearray(buffer)
 
-    return task
+    async def json(self):
+        return json.loads(await self.text())
 
-### fetch and response helpers
-async def _array_buffer(task, url):
-    buffer = await _buffer(task, url)
-    # works in Pyodide
-    if hasattr(buffer, "to_py"):
-        return buffer.to_py()
-    # shims in MicroPython
-    return memoryview(_as_bytearray(buffer))
+    async def text(self):
+        return await self._response.text()
 
-async def _blob(task, url):
-    response = await _response(task, url)
-    return await response.blob()
 
-async def _bytearray(task, url):
-    buffer = await _buffer(task, url)
-    return _as_bytearray(buffer)
-
-async def _json(task, url):
-    response = await _response(task, url)
-    return json.loads(await response.text())
-
-async def _text(task, url):
-    response = await _response(task, url)
-    return await response.text()
-
-### augmented fetch w/ direct access abilities
-def fetch(url, **kw):
+async def fetch(url, **kw):
+    # workaround Pyodide / MicroPython dict <-> js conversion
     options = js.JSON.parse(json.dumps(kw))
-    fetch = js.fetch(url, options)
-    # flag fetch response as unknown
-    fetch._response = None
-    # flag fetch response as known if directly awaited
-    awaited = fetch.then(lambda response: _awaited(fetch, response))
-    # augmented the promise providing direct access
-    awaited.arrayBuffer = lambda: _array_buffer(fetch, url)
-    awaited.blob = lambda: _blob(fetch, url)
-    awaited.bytearray = lambda: _bytearray(fetch, url)
-    awaited.json = lambda: _json(fetch, url)
-    awaited.text = lambda: _text(fetch, url)
-    return awaited
+    response = await js.fetch(url, options)
+    return _Response(response)
