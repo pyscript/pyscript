f4c4edeb29b97dd596af2f001c46d3e0a9576c02
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2024-08-07 12:33:20 +0200

    Implemented pyminify for our stdlib (#2140)
    * Implemented pyminify for our stdlib
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- pyscript.core/rollup/stdlib.cjs
+++ pyscript.core/rollup/stdlib.cjs
@@ -4,13 +4,27 @@ const {
     statSync,
     writeFileSync,
 } = require("node:fs");
+
+const { spawnSync } = require("node:child_process");
+
 const { join } = require("node:path");
 
 const crawl = (path, json) => {
     for (const file of readdirSync(path)) {
         const full = join(path, file);
-        if (/\.py$/.test(file)) json[file] = readFileSync(full).toString();
-        else if (statSync(full).isDirectory() && !file.endsWith("_"))
+        if (/\.py$/.test(file)) {
+            if (process.env.NO_MIN) json[file] = readFileSync(full).toString();
+            else {
+                const {
+                    output: [error, result],
+                } = spawnSync("pyminify", [
+                    "--remove-literal-statements",
+                    full,
+                ]);
+                if (error) process.exit(1);
+                json[file] = result.toString();
+            }
+        } else if (statSync(full).isDirectory() && !file.endsWith("_"))
             crawl(full, (json[file] = {}));
     }
 };
