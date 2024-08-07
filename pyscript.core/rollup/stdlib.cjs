b56d7b7a7ee43f82940a1f5899a993976dc77ad8
webreflection <andrea.giammarchi@gmail.com>
2024-08-07 10:22:41 +0200

    Implemented pyminify for our stdlib

--- pyscript.core/rollup/stdlib.cjs
+++ pyscript.core/rollup/stdlib.cjs
@@ -4,12 +4,24 @@ const {
     statSync,
     writeFileSync,
 } = require("node:fs");
+
+
+const { spawnSync } = require("node:child_process");
+
 const { join } = require("node:path");
 
 const crawl = (path, json) => {
     for (const file of readdirSync(path)) {
         const full = join(path, file);
-        if (/\.py$/.test(file)) json[file] = readFileSync(full).toString();
+        if (/\.py$/.test(file)) {
+            if (process.env.NO_MIN)
+                json[file] = readFileSync(full).toString();
+            else {
+                const { output: [error, result] } = spawnSync('pyminify', [full]);
+                if (error) process.exit(1);
+                json[file] = result.toString();
+            }
+        }
         else if (statSync(full).isDirectory() && !file.endsWith("_"))
             crawl(full, (json[file] = {}));
     }
