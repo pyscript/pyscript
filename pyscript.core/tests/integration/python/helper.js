e3c572d9e6a5f32315ad06b781862d9fa8b0b991
Nicholas H.Tollervey <ntoll@ntoll.org>
2024-09-10 16:21:49 +0100

    WiP pyscript.web tests pass with upytest.

---
+++ pyscript.core/tests/integration/python/helper.js
@@ -0,0 +1,18 @@
+const qs = new URLSearchParams(location.search);
+
+// src= to NOT have a config
+const src = qs.has('src') ? qs.get('src') : './main.py';
+
+// config= to NOT have a src
+const config = qs.has('config') ? qs.get('config') : './settings.json';
+
+// terminal=0 to NOT have a terminal
+const terminal = qs.get('terminal') !== '0';
+
+const script = document.createElement('script');
+script.type = qs.get('type') || 'mpy';
+if (src) script.src = src;
+if (config) script.setAttribute('config', config);
+script.toggleAttribute('terminal', terminal);
+
+document.write(script.outerHTML);
