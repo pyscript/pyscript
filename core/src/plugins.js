8de97a7e7ba8ded79039f42b8094c44ffea82066
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2024-09-30 14:03:33 +0200

    Updated coincident to use a local channel (#2195)
    * Updated coincident to use a local channel

--- core/src/plugins.js
+++ core/src/plugins.js
@@ -1,10 +1,23 @@
 // ⚠️ This file is an artifact: DO NOT MODIFY
 export default {
     ["deprecations-manager"]: () =>
-        import(/* webpackIgnore: true */ "./plugins/deprecations-manager.js"),
-    error: () => import(/* webpackIgnore: true */ "./plugins/error.js"),
+        import(
+            /* webpackIgnore: true */
+            "./plugins/deprecations-manager.js"
+        ),
+    error: () =>
+        import(
+            /* webpackIgnore: true */
+            "./plugins/error.js"
+        ),
     ["py-editor"]: () =>
-        import(/* webpackIgnore: true */ "./plugins/py-editor.js"),
+        import(
+            /* webpackIgnore: true */
+            "./plugins/py-editor.js"
+        ),
     ["py-terminal"]: () =>
-        import(/* webpackIgnore: true */ "./plugins/py-terminal.js"),
+        import(
+            /* webpackIgnore: true */
+            "./plugins/py-terminal.js"
+        ),
 };
--- core/tests/py_tests.spec.js
+++ core/tests/py_tests.spec.js
@@ -1,8 +1,9 @@
 import { test, expect } from '@playwright/test';
 
+test.setTimeout(120 * 1000);
+
 test('Python unit tests - MicroPython on MAIN thread', async ({ page }) => {
   await page.goto('http://localhost:8080/tests/python/index.html');
-  test.setTimeout(120*1000);  // Increase timeout for this test.
   const result = page.locator("#result");  // Payload for results will be here.
   await result.waitFor();  // wait for the result.
   const data = JSON.parse(await result.textContent());  // get the result data.
@@ -11,7 +12,6 @@ test('Python unit tests - MicroPython on MAIN thread', async ({ page }) => {
 
 test('Python unit tests - Pyodide on MAIN thread', async ({ page }) => {
   await page.goto('http://localhost:8080/tests/python/index.html?type=py');
-  test.setTimeout(120*1000);  // Increase timeout for this test.
   const result = page.locator("#result");  // Payload for results will be here.
   await result.waitFor();  // wait for the result.
   const data = JSON.parse(await result.textContent());  // get the result data.
@@ -20,7 +20,6 @@ test('Python unit tests - Pyodide on MAIN thread', async ({ page }) => {
 
 test('Python unit tests - MicroPython on WORKER', async ({ page }) => {
   await page.goto('http://localhost:8080/tests/python/index.html?worker');
-  test.setTimeout(120*1000);  // Increase timeout for this test.
   const result = page.locator("#result");  // Payload for results will be here.
   await result.waitFor();  // wait for the result.
   const data = JSON.parse(await result.textContent());  // get the result data.
@@ -29,7 +28,6 @@ test('Python unit tests - MicroPython on WORKER', async ({ page }) => {
 
 test('Python unit tests - Pyodide on WORKER', async ({ page }) => {
   await page.goto('http://localhost:8080/tests/python/index.html?type=py&worker');
-  test.setTimeout(120*1000);  // Increase timeout for this test.
   const result = page.locator("#result");  // Payload for results will be here.
   await result.waitFor();  // wait for the result.
   const data = JSON.parse(await result.textContent());  // get the result data.
