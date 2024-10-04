f827efe2fc02c20fa844ef7a1e1a1d0c6a7c0be7
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2024-10-04 17:06:07 +0200

    Add a "donkey" worker that execs or evaluates all the things (#2210)
    * WIP
    
    * Add a "donkey" worker that execs or evaluates all the things
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- core/types/plugins.d.ts
+++ core/types/plugins.d.ts
@@ -1,5 +1,6 @@
 declare const _default: {
     "deprecations-manager": () => Promise<typeof import("./plugins/deprecations-manager.js")>;
+    donkey: () => Promise<typeof import("./plugins/donkey.js")>;
     error: () => Promise<typeof import("./plugins/error.js")>;
     "py-editor": () => Promise<typeof import("./plugins/py-editor.js")>;
     "py-terminal": () => Promise<typeof import("./plugins/py-terminal.js")>;
---
+++ core/types/plugins/donkey.d.ts
@@ -0,0 +1,2 @@
+declare function _default(options?: {}): Promise<any>;
+export default _default;
