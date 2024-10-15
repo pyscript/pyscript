e11bd06753a633a5e84257247b842a8e27188b17
webreflection <andrea.giammarchi@gmail.com>
2024-10-15 12:47:28 +0200

    Donkey clear and reset now terminate when busy

--- core/types/plugins/donkey.d.ts
+++ core/types/plugins/donkey.d.ts
@@ -2,8 +2,8 @@ declare function _default(options?: {}): Promise<{
     process: (code: any) => Promise<any>;
     execute: (code: any) => Promise<any>;
     evaluate: (code: any) => Promise<any>;
-    clear: () => any;
-    reset: () => any;
+    clear: () => Promise<void>;
+    reset: () => Promise<void>;
     kill: () => void;
 }>;
 export default _default;
