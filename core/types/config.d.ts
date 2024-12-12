36661479dc770137007127ee3de9baf38bc860da
webreflection <andrea.giammarchi@gmail.com>
2024-12-12 12:51:42 +0100

    Introducing <script type="py-game">

--- core/types/config.d.ts
+++ core/types/config.d.ts
@@ -1,2 +1,7 @@
+export function configDetails(config: string, type: string | null): {
+    json: boolean;
+    toml: boolean;
+    text: string;
+};
 export const configs: Map<any, any>;
 export function relative_url(url: any, base?: string): string;
