a3767b5dfddd3b81fa38d914117ee3d879c23a41
webreflection <andrea.giammarchi@gmail.com>
2025-03-10 12:32:05 +0100

    Fix #2309 - Use all config options

--- core/tests/py_tests.worker.spec.js
+++ core/tests/py_tests.worker.spec.js
@@ -1,11 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test.setTimeout(120 * 1000);
+const timeout = 120 * 1000;
+
+test.setTimeout(timeout);
 
 test('Python unit tests - MicroPython on WORKER', async ({ page }) => {
   await page.goto('http://localhost:8080/tests/python/index.html?worker');
   const result = page.locator("#result");  // Payload for results will be here.
-  await result.waitFor();  // wait for the result.
+  await result.waitFor({ timeout });  // wait for the result.
   const data = JSON.parse(await result.textContent());  // get the result data.
   await expect(data.fails).toMatchObject([]);  // ensure no test failed.
 });
@@ -13,7 +15,7 @@ test('Python unit tests - MicroPython on WORKER', async ({ page }) => {
 test('Python unit tests - Pyodide on WORKER', async ({ page }) => {
   await page.goto('http://localhost:8080/tests/python/index.html?type=py&worker');
   const result = page.locator("#result");  // Payload for results will be here.
-  await result.waitFor();  // wait for the result.
+  await result.waitFor({ timeout });  // wait for the result.
   const data = JSON.parse(await result.textContent());  // get the result data.
   await expect(data.fails).toMatchObject([]);  // ensure no test failed.
 });
