403a39b00daab593cb81b11085fb598472f551f3
webreflection <andrea.giammarchi@gmail.com>
2025-04-07 17:24:54 +0200

    Fix #2326 - Redirect to localhost when on 0.0.0.0

---
+++ core/src/zero-redirect.js
@@ -0,0 +1,8 @@
+/* eslint no-unused-vars: 0 */
+try {
+  crypto.randomUUID();
+}
+catch (_) {
+  if (location.href.startsWith('http://0.0.0.0'))
+    location.href = location.href.replace('0.0.0.0', 'localhost');
+}
