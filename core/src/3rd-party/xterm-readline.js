7284f7f15f0678473c5ff4d042a577ac78a6c132
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2025-04-16 22:01:40 +0200

    Fix #2320 - Dispatch an event on code run in PyEditor (#2329)
    * Fix #2320 - Dispatch an event on code run in PyEditor

--- core/src/3rd-party/xterm-readline.js
+++ core/src/3rd-party/xterm-readline.js
@@ -1,5 +1,5 @@
 /**
- * Bundled by jsDelivr using Rollup v2.79.1 and Terser v5.19.2.
+ * Bundled by jsDelivr using Rollup v2.79.2 and Terser v5.39.0.
  * Original file: /npm/xterm-readline@1.1.2/lib/readline.js
  *
  * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
